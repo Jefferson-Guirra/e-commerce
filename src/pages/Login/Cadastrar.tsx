0d98b4df7275e9fc5158a7cb213a14c6703d5755
Jefferson Guirra <jeffersonloop14@gmail.com>
2023-01-03 00:35:37 -0300

    style: change styles error forms
    change styles erron in login form and create form

--- src/pages/Login/Cadastrar.tsx
+++ src/pages/Login/Cadastrar.tsx
@@ -1,37 +1,51 @@
-import useForm from "../../Hooks/useForm"
-import Input from "../../components/Input"
+import useForm from '../../Hooks/useForm'
+import Input from '../../components/Input'
 import { v4 as uuid } from 'uuid'
 import * as C from '../../styles/loginForm'
-import {GiBookshelf} from 'react-icons/gi'
-import {FcGoogle} from 'react-icons/fc'
-import { useRouter } from "next/router"
-import { useState } from "react"
-import Head from "next/head"
-import { VALIDATE_USER_CREATE,CREATE_USER } from "../../services/helper/FirebaseFunctions"
-import { GetServerSideProps } from "next"
-import { getSession } from "next-auth/react"
-import { SessionUser } from "../../Types/User"
+import { GiBookshelf } from 'react-icons/gi'
+import { FcGoogle } from 'react-icons/fc'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Head from 'next/head'
+import {
+  VALIDATE_USER_CREATE,
+  CREATE_USER
+} from '../../services/helper/FirebaseFunctions'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
+import { SessionUser } from '../../Types/User'
 
 const Cadastrar = () => {
   const router = useRouter()
   const username = useForm('')
   const email = useForm('email')
   const password = useForm('password')
-  const [error,setError] = useState< boolean | string>('')
+  const [error, setError] = useState<boolean | string>('')
+  const [loading, setLoading] = useState(false)
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      const validateInputs = email.validate() && password.validate()
-      const {error,validate} = await  VALIDATE_USER_CREATE({ username: username.value,email:email.value }).validateUser
-      if (validateInputs && validate) {
-        const keyUser = uuid()
-        await CREATE_USER({email:email.value,username:username.value,id:keyUser,password:password.value,})
-        router.push('/Login')
-      }
-      else if(validateInputs){
-        setError(error)
-      }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+    setError('')
+    const validateInputs = email.validate() && password.validate()
+    const { error, validate } = await VALIDATE_USER_CREATE({
+      username: username.value,
+      email: email.value
+    }).validateUser
+    if (validateInputs && validate) {
+      const keyUser = uuid()
+      await CREATE_USER({
+        email: email.value,
+        username: username.value,
+        id: keyUser,
+        password: password.value
+      })
+      router.push('/Login')
+    } else if (validateInputs) {
+      setError(error)
     }
+    setLoading(false)
+  }
   return (
     <>
       <Head>
@@ -39,7 +53,7 @@ const Cadastrar = () => {
       </Head>
       <C.container>
         <section id="create" className="content">
-          <GiBookshelf id='booksIcon'size={40} color="#001f3f" />
+          <GiBookshelf id="booksIcon" size={40} color="#001f3f" />
           <h1>Cadastrar</h1>
           <form onSubmit={handleSubmit}>
             <Input type="text" name="username" label="Usuário:" {...username} />
@@ -50,9 +64,16 @@ const Cadastrar = () => {
               label="Senha:"
               {...password}
             />
-            {error && <p className='userError'>{error}</p>}
-            <button type="submit">Entrar</button>
-            <button id="googleLogin"> <FcGoogle size={25} /> Entrar com Google</button>
+            {error && <p className="userErro">{error}</p>}
+            {!loading ? (
+              <button type="submit">Cadastrar</button>
+            ) : (
+              <button disabled type="submit">Cadastrando...</button>
+            )}
+            <button id="googleLogin">
+              {' '}
+              <FcGoogle size={25} /> Entrar com Google
+            </button>
           </form>
         </section>
       </C.container>
@@ -62,19 +83,17 @@ const Cadastrar = () => {
 
 export default Cadastrar
 
-export const getServerSideProps: GetServerSideProps = async({req})=>{
-  const session = await getSession({req}) as SessionUser
-  if(session?.id){
-    return{
-      redirect:{
-        destination:'/',
-        permanent:false
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = (await getSession({ req })) as SessionUser
+  if (session?.id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
       }
     }
   }
-  return{
-    props:{}
+  return {
+    props: {}
   }
 }
-
-
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,45 +1,49 @@
 import * as C from '../../styles/loginForm'
 import useForm from '../../Hooks/useForm'
 import Input from '../../components/Input'
-import {useState,useContext} from 'react'
+import { useState, useContext } from 'react'
 import { UserContext } from '../../UserContext'
-import {GiBookshelf} from 'react-icons/gi'
-import {FcGoogle} from 'react-icons/fc'
+import { GiBookshelf } from 'react-icons/gi'
+import { FcGoogle } from 'react-icons/fc'
 import { useRouter } from 'next/router'
 import { GET_USER } from '../../services/helper/FirebaseFunctions'
 import { getSession } from 'next-auth/react'
 import { SessionUser } from '../../Types/User'
-import { signIn} from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
-
-
 
 const Login = () => {
   const password = useForm('password')
   const username = useForm('')
   const router = useRouter()
   const email = useForm('email')
-  const [error,setError] = useState<boolean | string>(false)
-  const {createCookie,getPurchaseList} = useContext(UserContext)
-  
+  const [error, setError] = useState<boolean | string>(false)
+  const { createCookie, getPurchaseList } = useContext(UserContext)
+  const [loading, setLoading] = useState(false)
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setLoading(true)
+    setError(false)
     const validateInputs = email.validate() && password.validate()
-    const {error,validate,user} = await GET_USER(email.value,password.value).validateLogin
+    const { error, validate, user } = await GET_USER(
+      email.value,
+      password.value
+    ).validateLogin
 
-    if( validateInputs && validate && user ){
+    if (validateInputs && validate && user) {
       const userLogin = {
-        username:user.username,
-        token:user.id
+        username: user.username,
+        token: user.id
       }
-      createCookie('user',JSON.stringify(userLogin))
-      getPurchaseList({id:user.id,idCollection:'buyBooks'})
+      createCookie('user', JSON.stringify(userLogin))
+      getPurchaseList({ id: user.id, idCollection: 'buyBooks' })
       router.push('/')
-    }
-    else if(validateInputs){
+    } else if (validateInputs) {
       setError(error)
     }
+    setLoading(false)
   }
   return (
     <>
@@ -53,19 +57,34 @@ const Login = () => {
           <h1>Entrar</h1>
           <form onSubmit={handleSubmit}>
             <Input label="Email:" name="username" type="email" {...email} />
-            <Input label="Senha:" name="password" type="password" {...password} />
-            {error && !email.erro && !password.erro && <p className='userErro'>{error}</p>}
+            <Input
+              label="Senha:"
+              name="password"
+              type="password"
+              {...password}
+            />
+            {error && !email.erro && !password.erro && (
+              <p className="userErro">{error}</p>
+            )}
             <p>
               Ainda não possui conta?{' '}
               <span
-                style={{ color: '#001f3f', fontWeight:'bold', cursor:'pointer' }}
+                style={{
+                  color: '#001f3f',
+                  fontWeight: 'bold',
+                  cursor: 'pointer'
+                }}
                 onClick={() => router.push('/Login/Cadastrar')}
               >
                 Cadastre-se
               </span>
             </p>
-            <button type="submit">Entrar</button>
-            <button id="googleLogin" onClick={()=>signIn()}>
+            {!loading ? (
+              <button type="submit">Entrar</button>
+            ) : (
+              <button type="submit" disabled>Entando...</button>
+            )}
+            <button id="googleLogin" onClick={() => signIn()}>
               <FcGoogle size={25} /> Entrar com Google
             </button>
           </form>
@@ -77,21 +96,18 @@ const Login = () => {
 
 export default Login
 
-export const getServerSideProps :GetServerSideProps = async ({req})=>{
-  const session = await getSession({req}) as SessionUser
-  
-  if(session?.id){
-    return{
-      redirect:{
-        destination:'/',
-        permanent:false
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = (await getSession({ req })) as SessionUser
+
+  if (session?.id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
       }
     }
   }
   return {
-    props:{
-    }
+    props: {}
   }
 }
-
-
