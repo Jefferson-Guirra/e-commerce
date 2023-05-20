24a8a8605dddc515a9fe8da24823563091ef7d2a
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-20 17:32:25 -0300

    refactor: move creation Search page to features folder

--- src/pages/Search/[q].tsx
+++ src/pages/Search/[q].tsx
@@ -1,10 +1,8 @@
 import { GetServerSideProps } from 'next'
 import { useEffect, useState, useRef } from 'react'
 import { BOOKS_API, GET_BOOKS_PARAMS } from '../../Api'
-import { BiBookOpen } from 'react-icons/bi'
-import * as C from '../../styles/search'
 import Head from 'next/head'
-import Link from 'next/link'
+import { SearchContainer } from '../../features'
 
 type Params = {
   q: string
@@ -18,16 +16,11 @@ const Search = ({ books, q }: Props) => {
   const bookFormat: BOOKS_API = JSON.parse(books)
   const [bookResults, setBookResults] = useState(bookFormat)
   const title = q.replace(/\w+:/g, '')
-  const filter = useRef(0)
-  const maxPagination = Math.floor(bookFormat.totalItems / 15)
-  const pagination = Array.from(
-    { length: Number(maxPagination) },
-    (_, i = 1) => i + 1
-  )
+  const page = useRef(0)
 
   const handlePagination = async (value: number) => {
     const newBooks = await GET_BOOKS_PARAMS(q, value * 15, 15)
-    filter.current = value
+    page.current = value
     setBookResults(newBooks)
   }
 
@@ -40,42 +33,12 @@ const Search = ({ books, q }: Props) => {
       <Head>
         <title>{title}</title>
       </Head>
-      <C.container>
-        <div className="title">
-          <BiBookOpen size={40} />
-          <h1>{title}</h1>
-        </div>
-        <section className="containerBook">
-          {bookResults.books.map((item, index) => (
-            <article key={index} className="contentBook">
-              <Link href={`/Book/${item.id}`} className="cardBook">
-                <img
-                  src={`https://books.google.com/books/publisher/content/images/frontcover/${item.id}?fife=w340-h600&source=gbs_api`}
-                  alt={`Imagem do Livro ${item.title}`}
-                />
-                <div className="text">
-                  <p>{item.title}</p>
-                </div>
-                <button>{`A partir de R$ ${item.price}`}</button>
-              </Link>
-            </article>
-          ))}
-        </section>
-
-        <C.Pagination>
-          <div className="containerPagination">
-            {pagination
-              .filter(
-                (item) => item + 1 > filter.current && item < filter.current + 6
-              )
-              .map((item) => (
-                <button onClick={() => handlePagination(item - 1)} key={item}>
-                  {item}
-                </button>
-              ))}
-          </div>
-        </C.Pagination>
-      </C.container>
+      <SearchContainer
+        q={q}
+        books={bookResults}
+        page={page.current}
+        handlePagination={handlePagination}
+      />
     </>
   )
 }
