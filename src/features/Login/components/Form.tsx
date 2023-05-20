8f207ca8c5809aa8a08437d37cc5e06846511496
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-20 01:09:49 -0300

    feat: create login form component

---
+++ src/features/Login/components/Form.tsx
@@ -0,0 +1,81 @@
+import styles from './styles.module.css'
+import { useState } from 'react'
+import Input from '../../../components/Input'
+import useForm from '../../../Hooks/useForm'
+import { GET_USER } from '../../../services/helper/FirebaseFunctions'
+import { useRouter } from 'next/router'
+import { AiFillEye } from 'react-icons/ai'
+import { useUserContext } from '../../../UserContext'
+
+export const UserForm = () => {
+  const password = useForm('password')
+  const router = useRouter()
+  const email = useForm('email')
+  const [error, setError] = useState<boolean | string>(false)
+  const { createCookie, getPurchaseList } = useUserContext()
+  const [loading, setLoading] = useState(false)
+  const [hidden, setHidden] = useState(false)
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+    setError(false)
+    const validateInputs = email.validate() && password.validate()
+    const { error, validate, user } = await GET_USER(
+      email.value,
+      password.value
+    ).validateLogin
+
+    if (validateInputs && validate && user) {
+      const userLogin = {
+        username: user.username.replace(/\s\w+/g, ''),
+        token: user.id,
+      }
+      createCookie('user', JSON.stringify(userLogin))
+      getPurchaseList({ id: user.id, idCollection: 'buyBooks' })
+      router.push('/')
+    } else if (validateInputs) {
+      setError(error)
+    }
+    setLoading(false)
+  }
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <Input label="Email:" name="username" type="email" {...email} />
+
+      <div className={styles.passwordContainer}>
+        <Input
+          label="Senha:"
+          name="password"
+          type={!hidden ? 'password' : 'text'}
+          {...password}
+        />
+        <span id="passwordIcon" onClick={() => setHidden((state) => !state)}>
+          <AiFillEye size={25} color="#363636" />
+        </span>
+      </div>
+      {error && !email.erro && !password.erro && (
+        <p className="userErro">{error}</p>
+      )}
+      <p>
+        Ainda não possui conta?{' '}
+        <span
+          style={{
+            color: '#001f3f',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+          }}
+          onClick={() => router.push('/Login/Cadastrar')}
+        >
+          Cadastre-se
+        </span>
+      </p>
+      {!loading ? (
+        <button type="submit">Entrar</button>
+      ) : (
+        <button type="submit" disabled>
+          Entando...
+        </button>
+      )}
+    </form>
+  )
+}
