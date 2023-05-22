4bb83e99c489230dff80fffaf5937d575a96c1a1
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-22 00:17:40 -0300

    feat: change database address

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,16 +3,15 @@ import { getFirestore } from 'firebase/firestore'
 import 'firebase/firestore'
 
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBpV5nGZ4bcIuj2X9gwColFmPnm1ZmoyOs",
-  authDomain: "literando-52cd1.firebaseapp.com",
-  projectId: "literando-52cd1",
-  storageBucket: "literando-52cd1.appspot.com",
-  messagingSenderId: "238206507669",
-  appId: "1:238206507669:web:5b950a08bc1eee118f999c",
-  measurementId: "G-VRHZNMBSX6"
-};
+  apiKey: 'AIzaSyBEhiiLPwXiZEE8Gq_bWGO4EhuB8TwoloA',
+  authDomain: 'literando-ecommerce.firebaseapp.com',
+  projectId: 'literando-ecommerce',
+  storageBucket: 'literando-ecommerce.appspot.com',
+  messagingSenderId: '522541875641',
+  appId: '1:522541875641:web:89e9be6e70f67e5f69f668',
+  measurementId: 'G-BRF6J0T4T4',
+}
 
 
 const app = initializeApp(firebaseConfig);
