70733eab6a71c8ffdfea33f0c98c8936598915e0
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-22 23:07:59 -0300

    refactor: add BuyBookCard component

--- src/features/buy-list/pages/BuyBooks.tsx
+++ src/features/buy-list/pages/BuyBooks.tsx
@@ -1,15 +1,13 @@
 import styles from './styles.module.css'
 import { useState } from 'react'
-import { IoClose } from 'react-icons/io5'
 import { REMOVE_BOOK_DATABASE } from '../../../services/helper/FirebaseFunctions'
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from '../../../services/firebaseConnection'
 import PaypalAction from '../../../components/PaypalAction'
-import { MdAdd, MdRemove } from 'react-icons/md'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 import { useUserContext } from '../../../UserContext'
 import { IBuyBooksProps } from '../@types/IBuyBooksProps'
+import { BuyBookCard } from '../components'
 
 //Sandbox-id:AbJhKpgKw6gr0oH9PRqCr35jMcfKfaKYtRF_LGoDeOeiQhrsBsEsL_N_fXggNgGFnCFtyS55WsZJB4tI
 
@@ -19,6 +17,7 @@ export const BuyBooks = ({ books }: IBuyBooksProps) => {
   const router = useRouter()
   const { updatedBuyList, clearPurchaseList } = useUserContext()
   const price = bookList.reduce((acc, v) => acc + v.price * v.qtd, 0)
+
   const handleExclude = (id: string) => {
     updatedBuyList('remove')
     REMOVE_BOOK_DATABASE({ id, idCollection: 'buyBooks' })
@@ -65,77 +64,22 @@ export const BuyBooks = ({ books }: IBuyBooksProps) => {
       <section className={styles.content}>
         <h1>Meu Carrinho</h1>
         {bookList.map((item) => (
-          <article className={styles.cardContent} key={item.idDoc}>
-            <h2>{item.publisher}</h2>
-            <article className={styles.infoBook}>
-              <Link href={`/Book/${item.id}`} className={styles.img}>
-                <img
-                  src={`https://books.google.com/books/publisher/content/images/frontcover/${item.id}?fife=w340-h600&source=gbs_api`}
-                  height="150px"
-                  alt={`Imagem do livro ${item.title}`}
-                />
-              </Link>
-              <div className={styles.dataBook}>
-                <div className={styles.header}>
-                  <div className={styles.bookTitle}>
-                    <p>{item.title}</p>
-                  </div>
-                  <div className={styles.actions}>
-                    <button className={styles.qtd}>
-                      <MdRemove
-                        onClick={() => handlePrev(item.idDoc)}
-                        size={20}
-                        color="#363636"
-                      />
-                      <p>{item.qtd}</p>
-                      <MdAdd
-                        onClick={() => handleNext(item.idDoc)}
-                        size={20}
-                        color="#363636"
-                      />
-                    </button>
-                    <p>
-                      R$:{' '}
-                      {(item.price * item.qtd)
-                        .toFixed(2)
-                        .toString()
-                        .replace('.', ',')}
-                    </p>
-                    <button
-                      className={styles.btnExclude}
-                      onClick={() => handleExclude(item.idDoc)}
-                    >
-                      <IoClose size={20} color="#363636" />
-                    </button>
-                  </div>
-                </div>
-                <div className={styles.textInfo}>
-                  <p>Ano: </p>
-                  <span>{item.publisherDate.replace(/\-\d+/g, '')}</span>
-                </div>
-                <div className={styles.textInfo}>
-                  <p className={styles.textInfo}>Páginas: </p>
-                  <span>{item.pageCount}</span>
-                </div>
-                <div className={styles.textInfo}>
-                  <p>Idioma: </p>
-                  <span>{item.language}</span>
-                </div>
-              </div>
-            </article>
-
-            <article className={styles.buyInfoCard}>
-              <h2>Entrega Básica</h2>
-              <div className={styles.infoBuy}>
-                <span id="free">Frete grátis </span>
-                <p>neste vendedor nas compras a partir de</p>
-                <span>
-                  {' '}
-                  R$ {item.shipping.toFixed(2).toString().replace('.', ',')}
-                </span>
-              </div>
-            </article>
-          </article>
+          <BuyBookCard
+            handleExclude={handleExclude}
+            handleNext={handleNext}
+            handlePrev={handlePrev}
+            id={item.id}
+            idDoc={item.idDoc}
+            language={item.language}
+            pageCount={item.pageCount}
+            price={item.price}
+            publisher={item.publisher}
+            publisherDate={item.publisherDate}
+            qtd={item.qtd}
+            shipping={item.shipping}
+            title={item.title}
+            key={item.idDoc}
+          />
         ))}
       </section>
       <article className={styles.price}>
