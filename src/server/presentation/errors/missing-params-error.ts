1186659226d521540db72cfb7f294295b679ef94
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-26 02:35:22 -0300

    fix: create correct MissingParamsError

--- src/server/presentation/errors/missing-params-error.ts
+++ src/server/presentation/errors/missing-params-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor(paramName: string) {
-    super(`invalid param ${paramName}`)
-    this.name = 'invalid params error'
+    super(`missing param ${paramName}`)
+    this.name = 'missing params error'
   }
 }
