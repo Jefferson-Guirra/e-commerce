4df2e44ec97beda30adc56309e8e436ec71a8154
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-26 02:16:55 -0300

    feat: ensure SignupController call AddAccount method with correct values

--- src/server/presentation/controllers/signup/signup-controller.ts
+++ src/server/presentation/controllers/signup/signup-controller.ts
@@ -1,13 +1,20 @@
+import { AddAccountModel } from '../../../domain/usecases/add-account'
+import { AddAccount } from '../../../domain/usecases/add-account'
 import { badRequest } from '../../helpers/http'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Validation } from '../../protocols/validate'
 
 export class SignupController implements Controller {
-  constructor(private readonly validate: Validation) {}
+  constructor(
+    private readonly validate: Validation,
+    private readonly addAccount: AddAccount
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validate.validation(httpRequest)
     if (error) return badRequest(error)
+    const { password, email, username } = httpRequest.body
+    await this.addAccount.add({ username, email, password })
     return {
       statusCode: 200,
       body: 'succeeds',
