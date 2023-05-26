19d9336800eeca83e96644b8d4951571ad7bb808
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-26 01:13:07 -0300

    feat: ensure validation method is called with correct values

--- src/server/presentation/controllers/signup/signup-controller.ts
+++ src/server/presentation/controllers/signup/signup-controller.ts
@@ -1,16 +1,11 @@
-import { InvalidParamsError } from '../../errors/invalid-params-error'
-import { badRequest } from '../../helpers/http'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { Validation } from '../../protocols/validate'
 
 export class SignupController implements Controller {
+  constructor(private readonly validate: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validateFields = ['username', 'email', 'password']
-    for (const field of validateFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new InvalidParamsError(field))
-      }
-    }
+    this.validate.validation(httpRequest)
     return {
       statusCode: 200,
       body: 'succeeds',
