08e90f008aa72d7aadd473b2ebf8a2ff6cfbe326
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-26 00:32:26 -0300

    test: ensure SignupController returns InvalidParamsError if password is not provided

--- src/server/presentation/controllers/signup/signup.spec.ts
+++ src/server/presentation/controllers/signup/signup.spec.ts
@@ -22,4 +22,28 @@ describe('Signup Controller', () => {
     const response = await sut.handle(fakeRequest)
     expect(response).toEqual(badRequest(new InvalidParamsError('username')))
   })
+
+  test('should return InvalidParamsError is email not provided ', async () => {
+    const fakeRequest = {
+      body: {
+        username: 'any_username',
+        password: 'any_password',
+      },
+    }
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeRequest)
+    expect(response).toEqual(badRequest(new InvalidParamsError('email')))
+  })
+
+  test('should return InvalidParamsError is password not provided ', async () => {
+    const fakeRequest = {
+      body: {
+        username: 'any_username',
+        email: 'any_email@mail.com',
+      },
+    }
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeRequest)
+    expect(response).toEqual(badRequest(new InvalidParamsError('password')))
+  })
 })
