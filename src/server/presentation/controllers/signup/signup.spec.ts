2475b9599342e38bbb9d7206e0c8ea47ec2c5601
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-26 01:14:22 -0300

    test: ensure validation method is called with correct values

--- src/server/presentation/controllers/signup/signup.spec.ts
+++ src/server/presentation/controllers/signup/signup.spec.ts
@@ -1,49 +1,38 @@
-import { InvalidParamsError } from '../../errors/invalid-params-error'
-import { badRequest } from '../../helpers/http'
 import { SignupController } from './signup-controller'
+import { Validation } from '../../protocols/validate'
+import { HttpRequest } from '../../protocols/http'
 interface SutTypes {
   sut: SignupController
+  validationStub: Validation
+}
+const makeValidate = (): Validation => {
+  class ValidationStub implements Validation {
+    validation() {
+      return undefined
+    }
+  }
+  return new ValidationStub()
 }
 const makeSut = (): SutTypes => {
-  const sut = new SignupController()
+  const validationStub = makeValidate()
+  const sut = new SignupController(validationStub)
   return {
+    validationStub,
     sut,
   }
 }
 describe('Signup Controller', () => {
-  test('should return InvalidParamsError is username not provided ', async () => {
-    const fakeRequest = {
+  test('Should validation call with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validation')
+    const fakeAccount: HttpRequest = {
       body: {
+        username: 'any_username',
         email: 'any_email@mail.com',
         password: 'any_password',
       },
     }
-    const { sut } = makeSut()
-    const response = await sut.handle(fakeRequest)
-    expect(response).toEqual(badRequest(new InvalidParamsError('username')))
-  })
-
-  test('should return InvalidParamsError is email not provided ', async () => {
-    const fakeRequest = {
-      body: {
-        username: 'any_username',
-        password: 'any_password',
-      },
-    }
-    const { sut } = makeSut()
-    const response = await sut.handle(fakeRequest)
-    expect(response).toEqual(badRequest(new InvalidParamsError('email')))
-  })
-
-  test('should return InvalidParamsError is password not provided ', async () => {
-    const fakeRequest = {
-      body: {
-        username: 'any_username',
-        email: 'any_email@mail.com',
-      },
-    }
-    const { sut } = makeSut()
-    const response = await sut.handle(fakeRequest)
-    expect(response).toEqual(badRequest(new InvalidParamsError('password')))
+    await sut.handle(fakeAccount)
+    expect(validateSpy).toHaveBeenCalledWith(fakeAccount)
   })
 })
