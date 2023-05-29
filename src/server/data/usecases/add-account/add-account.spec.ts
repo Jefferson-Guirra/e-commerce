d138162bc2ec80a898c258f3a0162df1e2c58371
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-29 18:47:46 -0300

    test: ensure AddAccountRepository return throw if LoadByEmail return throw

--- src/server/data/usecases/add-account/add-account.spec.ts
+++ src/server/data/usecases/add-account/add-account.spec.ts
@@ -56,4 +56,13 @@ describe('AddAccountRepository', () => {
     const account = await sut.add(makeFakeAddAccount())
     expect(account).toBeFalsy()
   })
+
+  test('should return throw if loadByEmail return throw', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.add(makeFakeAddAccount())
+    await expect(promise).rejects.toThrow()
+  })
 })
