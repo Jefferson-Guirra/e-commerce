6dae60b321f80fca0dfbce8cb300f5c49e9c4e08
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-29 22:00:58 -0300

    test: ensure AccountMongoRepository return account if LoadByEmail success

---
+++ src/server/infra/db/account/account-mongo-repository.spec.ts
@@ -0,0 +1,33 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account-mongo-repository'
+
+const makeSut = () => new AccountMongoRepository()
+describe('AccountMongoRepository', () => {
+  let accountCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    MongoHelper.disconnect()
+  })
+
+  test('should return account loadByEmail success', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      username: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeTruthy()
+    expect(account?.id).toBeTruthy()
+    expect(account?.email).toBe('any_email@mail.com')
+    expect(account?.password).toBe('any_password')
+    expect(account?.username).toBe('any_name')
+  })
+})
