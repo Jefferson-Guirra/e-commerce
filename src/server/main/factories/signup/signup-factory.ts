d29181a0dcc41903e5b4621fd07d0b72338bae7e
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-30 19:11:13 -0300

    feat: add LogControllerDecorator in makeSignUpController factory method

--- src/server/main/factories/signup/signup-factory.ts
+++ src/server/main/factories/signup/signup-factory.ts
@@ -1,9 +1,11 @@
 import { DbAddAccountRepository } from '../../../data/usecases/add-account/db-add-account'
 import { BcrypterAdapter } from '../../../infra/criptography/bcrypt-adapter.ts/bcrypter-adapter'
 import { AccountMongoRepository } from '../../../infra/db/account/account-mongo-repository'
+import { LogMongoRepository } from '../../../infra/db/log/log-mongo-repository'
 import { SignupController } from '../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../presentation/protocols/controller'
 import { Validate } from '../../../presentation/validators/validate'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 
 export const makeSignUpController = (): Controller => {
   const validation = new Validate()
@@ -14,5 +16,10 @@ export const makeSignUpController = (): Controller => {
     bcrypterAdapter,
     accountMongoRepository
   )
-  return new SignupController(validation, addAccountRepository)
+  const logMongoRepository = new LogMongoRepository()
+  const signupController = new SignupController(
+    validation,
+    addAccountRepository
+  )
+  return new LogControllerDecorator(signupController, logMongoRepository)
 }
