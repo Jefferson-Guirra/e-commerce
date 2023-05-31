5738ba5d2cbe7c7f92df2da6d7e58259bd1476f8
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-30 22:42:48 -0300

    feat: ensure DbAuthentication call Encrypter with correct values

--- src/server/data/usecases/authentication/db-authentication.ts
+++ src/server/data/usecases/authentication/db-authentication.ts
@@ -5,12 +5,14 @@ import {
 import { Encrypter } from '../../protocols/criptography/encrypter'
 import { HashCompare } from '../../protocols/criptography/hash-compare'
 import { LoadAccountByEmailRepository } from '../../protocols/db/account/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../protocols/db/account/update-acess-token-repository'
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccount: LoadAccountByEmailRepository,
     private readonly hashCompare: HashCompare,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
   async auth(account: AuthenticationModel): Promise<string | null> {
     const loadAccount = await this.loadAccount.loadByEmail(account.email)
@@ -22,6 +24,10 @@ export class DbAuthentication implements Authentication {
 
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(loadAccount.id)
+        await this.updateAccessTokenRepository.update(
+          loadAccount.id,
+          accessToken
+        )
         return Promise.resolve(accessToken)
       }
     }
