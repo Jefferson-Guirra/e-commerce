4f3437791067f2df44829c80fab0494ac1c87f52
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-31 17:06:40 -0300

    feat: ensure LoginController return badRequest if validation return error

--- src/server/presentation/controllers/login/login-controller.ts
+++ src/server/presentation/controllers/login/login-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../helpers/http'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Validation } from '../../protocols/validate'
@@ -5,7 +6,10 @@ import { Validation } from '../../protocols/validate'
 export class LoginController implements Controller {
   constructor(private readonly validate: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validate.validation(httpRequest)
+    const error = this.validate.validation(httpRequest)
+    if (error) {
+      return badRequest(error)
+    }
     return await Promise.resolve({ statusCode: 200, body: 'any_body' })
   }
 }
