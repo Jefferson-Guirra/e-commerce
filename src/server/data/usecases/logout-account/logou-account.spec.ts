d6c02f1f70b4f6f16aa9308c72777c960a4f1272
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-02 08:39:51 -0300

    test: ensure DbLgoutAccount call LoadAccountByAccessToken with correct token

---
+++ src/server/data/usecases/logout-account/logou-account.spec.ts
@@ -0,0 +1,40 @@
+import {
+  LoadAccountByAccessToken,
+  accountLoginModel,
+} from '../../protocols/db/account/load-account-by-access-token'
+import { DbLogoutAccount } from './logout-account'
+
+const makeLoadAccountByAccessTokenStub = (): LoadAccountByAccessToken => {
+  class LoadAccountByAccessTokenStub implements LoadAccountByAccessToken {
+    async load(accessToken: string): Promise<accountLoginModel | null> {
+      return await Promise.resolve({
+        username: 'any_username',
+        password: 'any_password',
+        email: 'any_email@mail.com',
+        id: 'any_id',
+        accessToken: 'any_token',
+      })
+    }
+  }
+  return new LoadAccountByAccessTokenStub()
+}
+interface SutTypes {
+  loadAccountByAccessTokenStub: LoadAccountByAccessToken
+  sut: DbLogoutAccount
+}
+const makeSut = (): SutTypes => {
+  const loadAccountByAccessTokenStub = makeLoadAccountByAccessTokenStub()
+  const sut = new DbLogoutAccount(loadAccountByAccessTokenStub)
+  return {
+    loadAccountByAccessTokenStub,
+    sut,
+  }
+}
+describe('DbLogoutAccount', () => {
+  test('should call load with correct token', async () => {
+    const { sut, loadAccountByAccessTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByAccessTokenStub, 'load')
+    await sut.logout('any_token')
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
+})
