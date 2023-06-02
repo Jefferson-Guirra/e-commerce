c5d5af73a43b8c3c6f49b44c0183dfcec9b3afbd
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-02 11:04:05 -0300

    feat: create LogoutController to a factory method

---
+++ src/server/main/factories/logout/logout-controller.ts
@@ -0,0 +1,14 @@
+import { DbLogoutAccount } from '../../../data/usecases/logout-account/logout-account'
+import { AccountMongoRepository } from '../../../infra/db/account/account-mongo-repository'
+import { LogoutController } from '../../../presentation/controllers/logout/logout-controller'
+import { Controller } from '../../../presentation/protocols/controller'
+import { LogoutValidate } from '../../../presentation/validators/logout-validate'
+export const makeLogoutController = (): Controller => {
+  const logoutValidate = new LogoutValidate()
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbLogoutAccount = new DbLogoutAccount(
+    accountMongoRepository,
+    accountMongoRepository
+  )
+  return new LogoutController(logoutValidate, dbLogoutAccount)
+}
