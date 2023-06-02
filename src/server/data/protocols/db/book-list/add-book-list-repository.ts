44dfcb8c751bf62c933c763b74d761a06a59b335
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-02 20:25:26 -0300

    test: ensure DbAddBookList call addBookRepository with correct values

--- src/server/data/protocols/db/book-list/add-book-list-repository.ts
+++ src/server/data/protocols/db/book-list/add-book-list-repository.ts
@@ -1,5 +1,18 @@
-import { BookModel } from '../../../../domain/models/book'
+import { AddBookModel } from '../../../../domain/usecases/add-book-list'
 
+export interface AddBookRepositoryModel {
+  title: string
+  description: string
+  authors: string[]
+  price: number
+  language: string
+  publisher: string
+  publisherDate: string
+  imgUrl: string
+  date: number
+  userId: string
+  id: string
+}
 export interface AddBookListRepository {
-  addBook: (book: BookModel) => Promise<AddBookModel | null>
+  addBook: (book: AddBookRepositoryModel) => Promise<AddBookModel | null>
 }
--- src/server/data/usecases/add-book-list/db-add-book-list.ts
+++ src/server/data/usecases/add-book-list/db-add-book-list.ts
@@ -4,17 +4,30 @@ import {
   AddBookModel,
 } from '../../../domain/usecases/add-book-list'
 import { LoadAccountByAccessTokenRepository } from '../../protocols/db/account/load-account-by-access-token-repository'
+import { AddBookListRepository } from '../../protocols/db/book-list/add-book-list-repository'
+import { GetDate } from '../logout-account/protocols/get-date'
 
 export class DbAddBookList implements AddBookList {
   constructor(
-    private readonly loadAccount: LoadAccountByAccessTokenRepository
+    private readonly loadAccount: LoadAccountByAccessTokenRepository,
+    private readonly addBookListRepository: AddBookListRepository,
+    private readonly getDate: GetDate
   ) {}
   async add(book: BookModel): Promise<AddBookModel | undefined> {
-    const { accessToken, ...fields } = book
+    const { accessToken, bookId, ...bookFields } = book
     const account = await this.loadAccount.loadByAccessToken(accessToken)
     if (!account) {
       return
     }
+    const date = this.getDate.date()
+    const { id } = account
+    await this.addBookListRepository.addBook({
+      id: bookId,
+      date,
+      userId: id.toString(),
+      ...bookFields,
+    })
+
     return {
       title: 'any_title',
       description: 'any_description',
