1ee5a2c1ee51112a8d41185234bb55e86564d377
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-02 09:07:45 -0300

    feat: ensure DbLogoutAccount call RemoveAccessToken with correct token

--- src/server/data/usecases/logout-account/logout-account.ts
+++ src/server/data/usecases/logout-account/logout-account.ts
@@ -1,15 +1,18 @@
 import { AccountLogout } from '../../../domain/usecases/logout-account'
 import { LoadAccountByAccessToken } from '../../protocols/db/account/load-account-by-access-token'
+import { RemoveAccessTokenRepository } from '../../protocols/db/account/remove-access-token-repository'
 
 export class DbLogoutAccount implements AccountLogout {
   constructor(
-    private readonly loadAccountByAccessToken: LoadAccountByAccessToken
+    private readonly loadAccountByAccessToken: LoadAccountByAccessToken,
+    private readonly removeAccessToken: RemoveAccessTokenRepository
   ) {}
   async logout(accessToken: string): Promise<string | undefined> {
     const account = await this.loadAccountByAccessToken.load(accessToken)
     if (!account) {
       return
     }
+    this.removeAccessToken.remove(accessToken)
     return 'logout success'
   }
 }
