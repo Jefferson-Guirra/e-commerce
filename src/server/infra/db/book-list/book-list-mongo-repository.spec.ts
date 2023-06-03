9c8a46a42f094a156f789981656fe8a0531721ac
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-03 12:29:39 -0300

    test: ensure AddBookListMongoRepository return book if loadByQuery success

--- src/server/infra/db/book-list/book-list-mongo-repository.spec.ts
+++ src/server/infra/db/book-list/book-list-mongo-repository.spec.ts
@@ -42,16 +42,48 @@ describe('BookListMongoRepository', () => {
     const sut = makeSut()
     const book = await sut.addBook(makeFakeRequest())
     expect(book).toBeTruthy()
-    expect(book?.title).toBe('any_title'),
-      expect(book?.description).toBe('any_description'),
-      expect(book?.authors).toEqual(['any_author']),
-      expect(book?.price).toBe(0.0),
-      expect(book?.language).toBe('any_language'),
-      expect(book?.publisher).toBe('any_publisher'),
-      expect(book?.publisherDate).toBe('any_date'),
-      expect(book?.date).toBe(123456),
-      expect(book?.imgUrl).toBe('any_url'),
-      expect(book?.id).toBeTruthy()
+    expect(book?.title).toBe('any_title')
+    expect(book?.description).toBe('any_description')
+    expect(book?.authors).toEqual(['any_author'])
+    expect(book?.price).toBe(0.0)
+    expect(book?.language).toBe('any_language')
+    expect(book?.publisher).toBe('any_publisher')
+    expect(book?.publisherDate).toBe('any_date')
+    expect(book?.date).toBe(123456)
+    expect(book?.imgUrl).toBe('any_url')
+    expect(book?.id).toBeTruthy()
+    expect(book?.userId).toBe('any_user_id')
+    expect(book?.queryDoc).toBe('any_user_idany_id')
+  })
+
+  test('should return book if loadBookByQuery success', async () => {
+    const sut = makeSut()
+    await bookCollection.insertOne({
+      title: 'any_title',
+      description: 'any_description',
+      authors: ['any_author'],
+      price: 0.0,
+      language: 'any_language',
+      publisher: 'any_publisher',
+      publisherDate: 'any_date',
+      date: 123456,
+      imgUrl: 'any_url',
+      id: 'any_id',
+      userId: 'any_user_id',
+      queryDoc: 'any_user_idany_id',
+    })
+    const book = await sut.loadBookByQuery('any_user_idany_id')
+    expect(book).toBeTruthy()
+    expect(book?.title).toBe('any_title')
+    expect(book?.description).toBe('any_description')
+    expect(book?.authors).toEqual(['any_author'])
+    expect(book?.price).toBe(0.0)
+    expect(book?.language).toBe('any_language')
+    expect(book?.publisher).toBe('any_publisher')
+    expect(book?.publisherDate).toBe('any_date')
+    expect(book?.date).toBe(123456)
+    expect(book?.imgUrl).toBe('any_url')
+    expect(book?.id).toBeTruthy()
     expect(book?.userId).toBe('any_user_id')
     expect(book?.queryDoc).toBe('any_user_idany_id')
   })
