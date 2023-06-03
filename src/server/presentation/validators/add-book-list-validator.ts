e6a07a4d4a1d987d5e266061b586253e5a3c2413
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-03 10:13:14 -0300

    feat: ensure AddBookListValidator return error if price  not provided

--- src/server/presentation/validators/add-book-list-validator.ts
+++ src/server/presentation/validators/add-book-list-validator.ts
@@ -3,7 +3,7 @@ import { Validation } from '../protocols/validate'
 
 export class AddBookListValidator implements Validation {
   validation(input: any): Error | undefined {
-    const requiredFields = ['title', 'description', 'authors']
+    const requiredFields = ['title', 'description', 'authors', 'price']
     for (const field of requiredFields) {
       if (!input.body[field]) {
         return new MissingParamError(field)
