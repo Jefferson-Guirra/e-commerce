1e99cc3e1b2e70b66e5ada7a1c772dce54998efa
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-03 00:43:48 -0300

    test: ensure BookListMongoRepository return book if addBook on a succedds

--- src/server/infra/db/book-list/book-list-mongo-repository.spec.ts
+++ src/server/infra/db/book-list/book-list-mongo-repository.spec.ts
@@ -14,7 +14,7 @@ const makeFakeRequest = (): AddBookRepositoryModel => ({
   date: 123456,
   imgUrl: 'any_url',
   id: 'any_id',
-  userId: 'any_id',
+  userId: 'any_user_id',
 })
 const makeSut = () => new BookListMongoRepository()
 let bookCollection: Collection
@@ -29,12 +29,29 @@ describe('BookListMongoRepository', () => {
 
   beforeEach(async () => {
     bookCollection = await MongoHelper.getCollection('bookList')
-    bookCollection.deleteMany({})
+    await bookCollection.deleteMany({})
   })
-  test('should add book in BookLIstMongoRepository', async () => {
+  test('should add book in BookLIstMongoRepository if addBook success', async () => {
     const sut = makeSut()
     await sut.addBook(makeFakeRequest())
     const count = await bookCollection.countDocuments()
     expect(count).toBe(1)
+  })
+
+  test('should return book if addBook success', async () => {
+    const sut = makeSut()
+    const book = await sut.addBook(makeFakeRequest())
+    expect(book).toBeTruthy()
+    expect(book?.title).toBe('any_title'),
+      expect(book?.description).toBe('any_description'),
+      expect(book?.authors).toEqual(['any_author']),
+      expect(book?.price).toBe(0.0),
+      expect(book?.language).toBe('any_language'),
+      expect(book?.publisher).toBe('any_publisher'),
+      expect(book?.publisherDate).toBe('any_date'),
+      expect(book?.date).toBe(123456),
+      expect(book?.imgUrl).toBe('any_url'),
+      expect(book?.id).toBeTruthy()
+    expect(book?.queryDoc).toBe('any_user_idany_id')
   })
 })
