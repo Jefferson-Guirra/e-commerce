35ada21d44dc5a4960f76d1d867aa85de7d2d936
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-03 00:41:59 -0300

    feat: ensure BookListMongoRepository return book if addBook on a succedds

--- src/server/infra/db/book-list/book-list-mongo-repository.ts
+++ src/server/infra/db/book-list/book-list-mongo-repository.ts
@@ -9,9 +9,11 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class BookListMongoRepository implements AddBookListRepository {
   async addBook(book: AddBookRepositoryModel): Promise<AddBookModel | null> {
     const { id, userId, ...bookfields } = book
+    console.error(/^[A-F0-9]+$/i.test(userId + id))
     const bookListCollection = await MongoHelper.getCollection('bookList')
     const result = await bookListCollection.insertOne({
-      _id: new ObjectId(userId + id),
+      _id: new ObjectId(),
+      queryDoc: userId + id,
       ...bookfields,
     })
 
