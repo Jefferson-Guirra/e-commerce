21d0431e26874c2727861d30682dfc6d6b116e91
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-03 10:02:23 -0300

    feat: ensure AddBookListValidator return error if authors not provided

--- src/server/presentation/validators/add-book-list-validator.ts
+++ src/server/presentation/validators/add-book-list-validator.ts
@@ -3,7 +3,7 @@ import { Validation } from '../protocols/validate'
 
 export class AddBookListValidator implements Validation {
   validation(input: any): Error | undefined {
-    const requiredFields = ['title', 'description']
+    const requiredFields = ['title', 'description', 'authors']
     for (const field of requiredFields) {
       if (!input.body[field]) {
         return new MissingParamError(field)
