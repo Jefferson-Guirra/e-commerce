0c20cd2f52ba975978fa97fafa05dd3cc6fcc2d9
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-02 23:38:42 -0300

    feat: ensure DbAddBookList call createQuery doc with correct value

--- src/server/data/usecases/add-book-list/db-add-book-list.ts
+++ src/server/data/usecases/add-book-list/db-add-book-list.ts
@@ -3,15 +3,19 @@ import {
   AddBookList,
   AddBookModel,
 } from '../../../domain/usecases/add-book-list'
+import { ServerError } from '../../../presentation/errors/server-error'
 import { LoadAccountByAccessTokenRepository } from '../../protocols/db/account/load-account-by-access-token-repository'
 import { AddBookListRepository } from '../../protocols/db/book-list/add-book-list-repository'
-import { GetDate } from '../logout-account/protocols/get-date'
+import { LoadBookByQueryDocRepository } from '../../protocols/db/book-list/load-book-list-by-query-doc'
+import { CreateQueryDoc } from './protocols/create-query-doc'
+import { GetDate } from './protocols/get-date'
 
 export class DbAddBookList implements AddBookList {
   constructor(
     private readonly loadAccount: LoadAccountByAccessTokenRepository,
     private readonly addBookListRepository: AddBookListRepository,
-    private readonly getDate: GetDate
+    private readonly getDate: GetDate,
+    private createQueryDoc: CreateQueryDoc
   ) {}
   async add(book: BookModel): Promise<AddBookModel | undefined> {
     const { accessToken, bookId, ...bookFields } = book
@@ -19,14 +23,19 @@ export class DbAddBookList implements AddBookList {
     if (!account) {
       return
     }
-    const date = this.getDate.date()
+
     const { id } = account
+    const queryDoc = this.createQueryDoc.create(id.toString(), bookId)
     const addBook = await this.addBookListRepository.addBook({
       id: bookId,
-      date,
+      date: this.getDate.date(),
       userId: id.toString(),
       ...bookFields,
     })
+
+    if (!addBook) {
+      return Promise.reject(new ServerError())
+    }
 
     return addBook
   }
