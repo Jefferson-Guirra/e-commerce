deb8c754838a2e8c811165ead5877e7fe87390bf
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-03 09:56:15 -0300

    test: ensure AddBookListValidator return error if title not provided

---
+++ src/server/presentation/validators/add-book-list-validator.spec.ts
@@ -0,0 +1,33 @@
+import { AddBookListValidator } from './add-book-list-validator'
+import { MissingParamError } from '../errors/missing-params-error'
+import { BookModel } from '../../domain/models/book'
+import { HttpRequest } from '../protocols/http'
+interface SutTypes {
+  sut: AddBookListValidator
+}
+const makeSut = (): SutTypes => {
+  const sut = new AddBookListValidator()
+  return {
+    sut,
+  }
+}
+describe('Signup Controller', () => {
+  test('should return MissingParamError is title not provided ', async () => {
+    const fakeRequest: HttpRequest = {
+      body: {
+        description: 'any_description',
+        authors: ['any_author'],
+        price: 0.0,
+        language: 'any_language',
+        publisher: 'any_publisher',
+        publisherDate: 'any_date',
+        imgUrl: 'any_url',
+        accessToken: 'any_token',
+        bookId: 'any_id',
+      },
+    }
+    const { sut } = makeSut()
+    const response = sut.validation(fakeRequest)
+    expect(response).toEqual(new MissingParamError('title'))
+  })
+})
