1c8bbd76fc5d43b0b66df168ea2dbf758f1301ff
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-06 20:11:17 -0300

    feat: ensure DbRemoveBookList return a book on a succeeds

--- src/server/data/usecases/book-list/remove-book-list/db-remove-book-list.ts
+++ src/server/data/usecases/book-list/remove-book-list/db-remove-book-list.ts
@@ -11,28 +11,15 @@ export class DbRemoveBookList implements RemoveBookList {
   async remove(
     accessToken: string,
     idBook: string
-  ): Promise<AddBookModel | undefined> {
+  ): Promise<AddBookModel | null> {
     const account = await this.loadAccount.loadByAccessToken(accessToken)
 
     if (!account) {
-      return undefined
+      return null
     }
 
     const { id } = account
-    await this.removeBook.remove(id, idBook)
-    return {
-      title: 'any_title',
-      description: 'any_description',
-      authors: ['any_author'],
-      price: 0.0,
-      language: 'any_language',
-      publisher: 'any_publisher',
-      date: 1254632254,
-      publisherDate: 'any_date',
-      imgUrl: 'any_url',
-      id: 'any_id',
-      userId: 'any_user_id',
-      queryDoc: 'any_user_idany_id',
-    }
+    const removeBook = await this.removeBook.remove(id, idBook)
+    return removeBook
   }
 }
