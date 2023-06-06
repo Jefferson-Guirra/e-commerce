d80efbb76eb273d119f52bd44be022447dc0e080
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-06 18:08:22 -0300

    feat: ensure RemoveBookListController call RemoveBook with correct values

---
+++ src/server/domain/usecases/book-list/remove-book-list.ts
@@ -0,0 +1,8 @@
+import { AddBookModel } from './add-book-list'
+
+export interface RemoveBookList {
+  remove: (
+    accessToken: string,
+    idBook: string
+  ) => Promise<AddBookModel | undefined>
+}
--- src/server/presentation/controllers/remove-book-list/remove-book-list-controller.ts
+++ src/server/presentation/controllers/remove-book-list/remove-book-list-controller.ts
@@ -1,15 +1,21 @@
+import { RemoveBookList } from '../../../domain/usecases/book-list/remove-book-list'
 import { badRequest, ok } from '../../helpers/http'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Validation } from '../../protocols/validate'
 
 export class RemoveBookListController implements Controller {
-  constructor(private readonly validate: Validation) {}
+  constructor(
+    private readonly validate: Validation,
+    private readonly removeBook: RemoveBookList
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validate.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken, idBook } = httpRequest.body
+    await this.removeBook.remove(accessToken, idBook)
     return ok('success')
   }
 }
