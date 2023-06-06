e8a7b29b4a522ce113be62e613f409bff9039653
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-06 19:52:15 -0300

    feat: ensure DbRemoveBookList call RemoveBook with correct values

--- src/server/data/usecases/book-list/remove-book-list/db-remove-book-list.ts
+++ src/server/data/usecases/book-list/remove-book-list/db-remove-book-list.ts
@@ -1,10 +1,12 @@
 import { AddBookModel } from '../../../../domain/usecases/book-list/add-book-list'
 import { RemoveBookList } from '../../../../domain/usecases/book-list/remove-book-list'
 import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { RemoveBookListRepository } from '../../../protocols/db/book-list/remove-book-list'
 
 export class DbRemoveBookList implements RemoveBookList {
   constructor(
-    private readonly loadAccount: LoadAccountByAccessTokenRepository
+    private readonly loadAccount: LoadAccountByAccessTokenRepository,
+    private readonly removeBook: RemoveBookListRepository
   ) {}
   async remove(
     accessToken: string,
@@ -15,6 +17,9 @@ export class DbRemoveBookList implements RemoveBookList {
     if (!account) {
       return undefined
     }
+
+    const { id } = account
+    await this.removeBook.remove(id, idBook)
     return {
       title: 'any_title',
       description: 'any_description',
