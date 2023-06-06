365acd851289f3cda20e2a1a0c43aa7be7441e04
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-06 19:09:32 -0300

    feat: ensure DbRemoveBookList return undefined if LoadAccount return null

--- src/server/data/usecases/book-list/remove-book-list/db-remove-book-list.ts
+++ src/server/data/usecases/book-list/remove-book-list/db-remove-book-list.ts
@@ -10,7 +10,24 @@ export class DbRemoveBookList implements RemoveBookList {
     accessToken: string,
     idBook: string
   ): Promise<AddBookModel | undefined> {
-    await this.loadAccount.loadByAccessToken(accessToken)
-    return
+    const account = await this.loadAccount.loadByAccessToken(accessToken)
+
+    if (!account) {
+      return undefined
+    }
+    return {
+      title: 'any_title',
+      description: 'any_description',
+      authors: ['any_author'],
+      price: 0.0,
+      language: 'any_language',
+      publisher: 'any_publisher',
+      date: 1254632254,
+      publisherDate: 'any_date',
+      imgUrl: 'any_url',
+      id: 'any_id',
+      userId: 'any_user_id',
+      queryDoc: 'any_user_idany_id',
+    }
   }
 }
