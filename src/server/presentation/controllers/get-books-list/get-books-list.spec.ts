9d316f1b0fc548c3cf4fec178963fce00aab8039
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-07 13:20:42 -0300

    test: ensure GetBooksLisController call GetBooks wit correct accessToken

--- src/server/presentation/controllers/get-books-list/get-books-list.spec.ts
+++ src/server/presentation/controllers/get-books-list/get-books-list.spec.ts
@@ -1,3 +1,5 @@
+import { AddBookModel } from '../../../domain/usecases/book-list/add-book-list'
+import { GetBooksList } from '../../../domain/usecases/book-list/get-books-list'
 import { MissingParamError } from '../../errors/missing-params-error'
 import { badRequest } from '../../helpers/http'
 import { HttpRequest } from '../../protocols/http'
@@ -17,16 +19,49 @@ const makeValidateStub = (): Validation => {
   }
   return new ValidateStub()
 }
+
+const makeFakeAddBookModel = (): AddBookModel => {
+  return {
+    title: 'any_title',
+    description: 'any_description',
+    authors: ['any_author'],
+    price: 0.0,
+    language: 'any_language',
+    publisher: 'any_publisher',
+    date: 1254632254,
+    publisherDate: 'any_date',
+    imgUrl: 'any_url',
+    id: 'any_id',
+    userId: 'any_user_id',
+    queryDoc: 'any_user_idany_id',
+  }
+}
+
+const makeGetBooksListStub = (): GetBooksList => {
+  class GetBooksListStub implements GetBooksList {
+    async getBooks(accessToken: string): Promise<AddBookModel[] | null> {
+      return await Promise.resolve([
+        makeFakeAddBookModel(),
+        makeFakeAddBookModel(),
+      ])
+    }
+  }
+
+  return new GetBooksListStub()
+}
 interface SutTypes {
   validateStub: Validation
+  getBooksListStub: GetBooksList
   sut: GetBooksListController
 }
 
 const makeSut = (): SutTypes => {
   const validateStub = makeValidateStub()
-  const sut = new GetBooksListController(validateStub)
+  const getBooksListStub = makeGetBooksListStub()
+  const sut = new GetBooksListController(validateStub, getBooksListStub)
   return {
     validateStub,
+    getBooksListStub,
     sut,
   }
 }
@@ -45,5 +80,12 @@ describe('GetBookList', () => {
       .mockReturnValueOnce(new MissingParamError('any_field'))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('should call getBooks with correct accessToken', async () => {
+    const { sut, getBooksListStub } = makeSut()
+    const getBooksSpy = jest.spyOn(getBooksListStub, 'getBooks')
+    await sut.handle(makeFakeRequest())
+    expect(getBooksSpy).toHaveBeenCalledWith('any_token')
   })
 })
