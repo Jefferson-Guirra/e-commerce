2efedec814424c36262fa2d1587a3f2ab087a467
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-07 13:28:33 -0300

    test: ensure getBooksListController return 500 if getBooks fails

--- src/server/presentation/controllers/get-books-list/get-books-list.spec.ts
+++ src/server/presentation/controllers/get-books-list/get-books-list.spec.ts
@@ -1,7 +1,7 @@
 import { AddBookModel } from '../../../domain/usecases/book-list/add-book-list'
 import { GetBooksList } from '../../../domain/usecases/book-list/get-books-list'
 import { MissingParamError } from '../../errors/missing-params-error'
-import { badRequest } from '../../helpers/http'
+import { badRequest, serverError } from '../../helpers/http'
 import { HttpRequest } from '../../protocols/http'
 import { Validation } from '../../protocols/validate'
 import { GetBooksListController } from './get-books-list-controller'
@@ -87,5 +87,14 @@ describe('GetBookList', () => {
     const getBooksSpy = jest.spyOn(getBooksListStub, 'getBooks')
     await sut.handle(makeFakeRequest())
     expect(getBooksSpy).toHaveBeenCalledWith('any_token')
+  })
+
+  test('should return 500 if getBooks fails', async () => {
+    const { sut, getBooksListStub } = makeSut()
+    jest
+      .spyOn(getBooksListStub, 'getBooks')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(serverError())
   })
 })
