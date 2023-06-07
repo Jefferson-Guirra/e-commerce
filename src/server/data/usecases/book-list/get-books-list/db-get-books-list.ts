5b149095ce69964ab574a3571527b9e123e4e786
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-07 14:04:45 -0300

    feat: ensure DbGetBooksList return null if LoadAccountByAccessTokenRepository return null

--- src/server/data/usecases/book-list/get-books-list/db-get-books-list.ts
+++ src/server/data/usecases/book-list/get-books-list/db-get-books-list.ts
@@ -7,7 +7,25 @@ export class DbGetBooksList implements GetBooksList {
     private readonly loadAccount: LoadAccountByAccessTokenRepository
   ) {}
   async getBooks(accessToken: string): Promise<AddBookModel[] | null> {
-    await this.loadAccount.loadByAccessToken(accessToken)
-    return null
+    const account = await this.loadAccount.loadByAccessToken(accessToken)
+    if (!account) {
+      return null
+    }
+    return [
+      {
+        title: 'any_title',
+        description: 'any_description',
+        authors: ['any_author'],
+        price: 0.0,
+        language: 'any_language',
+        publisher: 'any_publisher',
+        date: 1254632254,
+        publisherDate: 'any_date',
+        imgUrl: 'any_url',
+        id: 'any_id',
+        userId: 'any_user_id',
+        queryDoc: 'any_user_idany_id',
+      },
+    ]
   }
 }
