263c6fa2c79cb103c2f27baeb69bc07d420dd551
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-07 13:27:09 -0300

    feat: ensure getBooksListController return 500 if getBooks fails

--- src/server/presentation/controllers/get-books-list/get-books-list-controller.ts
+++ src/server/presentation/controllers/get-books-list/get-books-list-controller.ts
@@ -1,5 +1,5 @@
 import { GetBooksList } from '../../../domain/usecases/book-list/get-books-list'
-import { badRequest, ok } from '../../helpers/http'
+import { badRequest, ok, serverError } from '../../helpers/http'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { Validation } from '../../protocols/validate'
@@ -10,12 +10,16 @@ export class GetBooksListController implements Controller {
     private readonly getBooksList: GetBooksList
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validate.validation(httpRequest)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validate.validation(httpRequest)
+      if (error) {
+        return badRequest(error)
+      }
+      const { accessToken } = httpRequest.body
+      await this.getBooksList.getBooks(accessToken)
+      return Promise.resolve(ok('success'))
+    } catch (err) {
+      return serverError(err as Error)
     }
-    const { accessToken } = httpRequest.body
-    await this.getBooksList.getBooks(accessToken)
-    return Promise.resolve(ok('success'))
   }
 }
