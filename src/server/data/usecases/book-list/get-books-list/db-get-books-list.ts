45df47118962424eeb97985ec5175633c63c97c1
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-07 15:46:41 -0300

    feat: ensure DbGetBooksList return books on a succeeds

--- src/server/data/usecases/book-list/get-books-list/db-get-books-list.ts
+++ src/server/data/usecases/book-list/get-books-list/db-get-books-list.ts
@@ -14,22 +14,7 @@ export class DbGetBooksList implements GetBooksList {
       return null
     }
     const { id } = account
-    await this.getBooksRepository.getBooks(id)
-    return [
-      {
-        title: 'any_title',
-        description: 'any_description',
-        authors: ['any_author'],
-        price: 0.0,
-        language: 'any_language',
-        publisher: 'any_publisher',
-        date: 1254632254,
-        publisherDate: 'any_date',
-        imgUrl: 'any_url',
-        id: 'any_id',
-        userId: 'any_user_id',
-        queryDoc: 'any_user_idany_id',
-      },
-    ]
+    const books = await this.getBooksRepository.getBooks(id)
+    return books
   }
 }
