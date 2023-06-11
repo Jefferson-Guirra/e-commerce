b9db45373f77bf2a14e904a26cf7a3563c0cb518
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-11 18:49:22 -0300

    feat: ensure DbAddBookBuyList return undefined if LadAccount return null

--- src/server/data/usecases/book-buy-list/db-add-book-buy-list.ts
+++ src/server/data/usecases/book-buy-list/db-add-book-buy-list.ts
@@ -10,7 +10,25 @@ export class DbAddBookBuyList implements AddBookBuyList {
     private readonly loadAccount: LoadAccountByAccessTokenRepository
   ) {}
   async add(book: BookModel): Promise<AddBuyBookModel | undefined> {
-    await this.loadAccount.loadByAccessToken(book.accessToken)
-    return
+    const account = await this.loadAccount.loadByAccessToken(book.accessToken)
+
+    if (!account) {
+      return
+    }
+    return {
+      authors: ['any_author'],
+      amount: 0,
+      date: 0,
+      description: 'any_description',
+      title: 'any_title',
+      id: 'any_id',
+      imgUrl: 'any_url',
+      language: 'any-language',
+      price: 0,
+      publisher: 'any_publisher',
+      publisherDate: 'any_date',
+      queryDoc: 'any_id_doc',
+      userId: 'any_user_id',
+    }
   }
 }
