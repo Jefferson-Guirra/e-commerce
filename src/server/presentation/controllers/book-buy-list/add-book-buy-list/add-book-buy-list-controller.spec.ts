804c21c88a80b3a30e1957448d9990841543c70d
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-11 18:15:46 -0300

    test: ensure AddBookBuyListController return a book on succeeds

--- src/server/presentation/controllers/book-buy-list/add-book-buy-list/add-book-buy-list-controller.spec.ts
+++ src/server/presentation/controllers/book-buy-list/add-book-buy-list/add-book-buy-list-controller.spec.ts
@@ -4,7 +4,12 @@ import {
   AddBuyBookModel,
 } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
 import { MissingParamError } from '../../../errors/missing-params-error'
-import { badRequest, serverError, unauthorized } from '../../../helpers/http'
+import {
+  badRequest,
+  ok,
+  serverError,
+  unauthorized,
+} from '../../../helpers/http'
 import { HttpRequest } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 import { AddBookBuyListController } from './add-book-buy-list-controller'
@@ -117,5 +122,11 @@ describe('AddBookBuyListController', () => {
       .mockReturnValueOnce(Promise.reject(new Error()))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(serverError())
+  })
+
+  test('should return 200 on a succeeds', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(ok(makeFakeAddBuyBook()))
   })
 })
