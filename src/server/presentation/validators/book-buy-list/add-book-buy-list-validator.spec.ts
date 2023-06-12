dc8bc023d55c8919e377ee6e45d3ee945661c55e
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-12 12:50:02 -0300

    test: ensure AddBookBuyList validator return MissingParams error if accessToken not provided

---
+++ src/server/presentation/validators/book-buy-list/add-book-buy-list-validator.spec.ts
@@ -0,0 +1,32 @@
+import { BookModel } from '../../../domain/models/book/book'
+import { MissingParamError } from '../../errors/missing-params-error'
+import { HttpRequest } from '../../protocols/http'
+import { AddBookBuyListValidator } from './add-book-buy-list-validator'
+
+const makeFakeRequest = (field: string): HttpRequest => {
+  const body: any = {
+    accessToken: 'any_token',
+    bookId: 'any_id',
+    authors: ['any_author'],
+    description: 'any_description',
+    title: 'any_title',
+    imgUrl: 'any_url',
+    language: 'any_language',
+    price: 0,
+    publisher: 'any_publisher',
+    publisherDate: 'any_date',
+  }
+
+  delete body[field]
+  return {
+    body,
+  }
+}
+const makeSut = (): AddBookBuyListValidator => new AddBookBuyListValidator()
+describe('AddBookBuyList', () => {
+  test('should return MissingParamsError if title not provided', () => {
+    const sut = makeSut()
+    const response = sut.validation(makeFakeRequest('accessToken'))
+    expect(response).toEqual(new MissingParamError('accessToken'))
+  })
+})
