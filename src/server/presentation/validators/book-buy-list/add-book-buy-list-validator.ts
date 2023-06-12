d546c207027b53a12c9aaaff286c9d2b81bc9359
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-12 12:59:44 -0300

    feat: ensure AddBookBuyListValidator return MissingParams error if description not provided

--- src/server/presentation/validators/book-buy-list/add-book-buy-list-validator.ts
+++ src/server/presentation/validators/book-buy-list/add-book-buy-list-validator.ts
@@ -4,7 +4,7 @@ import { Validation } from '../../protocols/validate'
 export class AddBookBuyListValidator implements Validation {
   validation(input: any): Error | undefined {
     const fieldArray = Object.keys(input?.body)
-    const requiredFields = ['accessToken', 'bookId', 'authors']
+    const requiredFields = ['accessToken', 'bookId', 'authors', 'description']
     for (const field of requiredFields) {
       if (!fieldArray.includes(field)) {
         return new MissingParamError(field)
