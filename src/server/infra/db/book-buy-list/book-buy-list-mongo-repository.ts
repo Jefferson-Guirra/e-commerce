092dfc12017f45cc205a46f8ddb8545d2e4649d8
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-12 12:33:02 -0300

    feat: ensure BuyBooksListMongoRepository return book if UpdateAmount success

--- src/server/infra/db/book-buy-list/book-buy-list-mongo-repository.ts
+++ src/server/infra/db/book-buy-list/book-buy-list-mongo-repository.ts
@@ -1,11 +1,15 @@
 import { AddBuyBookRepository } from '../../../data/protocols/db/book-buy-list/add-book-buy-list-repository'
 import { LoadBuyBookByQueryDocRepository } from '../../../data/protocols/db/book-buy-list/load-book-buy-list-by-query-doc-repository'
+import { UpdateBuyBookRepository } from '../../../data/protocols/db/book-buy-list/update-book-buy-list-repository'
 import { BookModel } from '../../../domain/models/book/book'
 import { AddBuyBookModel } from '../../../domain/usecases/book-buy-list/add-book-buy-list'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class BuyBooksListMongoRepository
-  implements AddBuyBookRepository, LoadBuyBookByQueryDocRepository
+  implements
+    AddBuyBookRepository,
+    LoadBuyBookByQueryDocRepository,
+    UpdateBuyBookRepository
 {
   async addBook(
     book: BookModel,
@@ -31,5 +35,18 @@ export class BuyBooksListMongoRepository
     const buyBooksCollection = await MongoHelper.getCollection('buyBooksList')
     const book = await buyBooksCollection.findOne({ queryDoc: userId + bookId })
     return book && MongoHelper.Map(book)
+  }
+
+  async updateAmount(book: AddBuyBookModel): Promise<AddBuyBookModel | null> {
+    const buyBookCollection = await MongoHelper.getCollection('buyBooksList')
+    const updateBook = await buyBookCollection.updateOne(
+      { queryDoc: book.queryDoc },
+      {
+        $set: {
+          amount: book.amount + 1,
+        },
+      }
+    )
+    return await this.loadBookByQueryDoc(book.userId, book.bookId)
   }
 }
