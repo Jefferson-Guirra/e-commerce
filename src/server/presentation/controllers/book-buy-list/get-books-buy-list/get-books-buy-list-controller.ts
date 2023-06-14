c3cce3585d3aa274a3084622ccc4c44f5439e3f0
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 18:25:57 -0300

    feat: ensure GetBuyBooksController call getBooks with correct accessToken

--- src/server/presentation/controllers/book-buy-list/get-books-buy-list/get-books-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/get-books-buy-list/get-books-buy-list-controller.ts
@@ -1,15 +1,21 @@
+import { GetBuyBooks } from '../../../../domain/usecases/book-buy-list/get-books-buy-list'
 import { badRequest, ok } from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 
 export class GetBuyBooksController implements Controller {
-  constructor(private readonly validator: Validation) {}
+  constructor(
+    private readonly validator: Validation,
+    private readonly getBooks: GetBuyBooks
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken } = httpRequest.body
+    await this.getBooks.getBuyBooks(accessToken)
     return ok('success')
   }
 }
