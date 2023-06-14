45e23b3b13e8b43f4aa6c2c814f6bc54b3fe0125
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 11:09:54 -0300

    feat: create RemoveAmountBuyBookListController to a factory method

---
+++ src/server/main/factories/book-buy-list/remove-amount-book-buy-list-factory.ts
@@ -0,0 +1,18 @@
+import { DbRemoveAmountBookBuyList } from '../../../data/usecases/book-buy-list/remove-amount-book-buy-list/db-remove-amount-book-buy-list'
+import { AccountMongoRepository } from '../../../infra/db/account/account-mongo-repository'
+import { BuyBooksListMongoRepository } from '../../../infra/db/book-buy-list/book-buy-list-mongo-repository'
+import { RemoveAmountBuyBookListController } from '../../../presentation/controllers/book-buy-list/remove-book-buy-list/remove-amount-book-buy-list-controller'
+import { Controller } from '../../../presentation/protocols/controller'
+import { RemoveAmountBuyBookValidator } from '../../../presentation/validators/book-buy-list/remove-book-amount-buy-list-validator'
+
+export const makeRemoveAmountBookBuyListController = (): Controller => {
+  const validator = new RemoveAmountBuyBookValidator()
+  const accountMongoRepository = new AccountMongoRepository()
+  const buyBookListMongoRepository = new BuyBooksListMongoRepository()
+  const dbRemoveAmountBuyBook = new DbRemoveAmountBookBuyList(
+    accountMongoRepository,
+    buyBookListMongoRepository,
+    buyBookListMongoRepository
+  )
+  return new RemoveAmountBuyBookListController(validator, dbRemoveAmountBuyBook)
+}
