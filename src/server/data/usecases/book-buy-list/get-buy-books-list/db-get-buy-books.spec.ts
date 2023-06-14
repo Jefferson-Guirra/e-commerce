193e981c88ecb51ac373172dac0e18033df37e6c
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 19:17:05 -0300

    test: ensure DbGetBuyBooks call GetBuyBooksRepository with correct id

--- src/server/data/usecases/book-buy-list/get-buy-books-list/db-get-buy-books.spec.ts
+++ src/server/data/usecases/book-buy-list/get-buy-books-list/db-get-buy-books.spec.ts
@@ -1,8 +1,27 @@
+import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
 import {
   LoadAccountByAccessTokenRepository,
   accountLoginModel,
 } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { GetBuyBooksRepository } from '../../../protocols/db/book-buy-list/get-books-buy-list-repository'
 import { DbGetBuyBooks } from './db-get-buy-books-list'
+
+const makeFakeAddBuyBook = (): AddBuyBookModel => ({
+  authors: ['any_author'],
+  amount: 0,
+  date: 0,
+  description: 'any_description',
+  title: 'any_title',
+  bookId: 'any_book_id',
+  id: 'any_id',
+  imgUrl: 'any_url',
+  language: 'any-language',
+  price: 0,
+  publisher: 'any_publisher',
+  publisherDate: 'any_date',
+  queryDoc: 'any_id_doc',
+  userId: 'any_user_id',
+})
 
 const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
   class LoadAccountByAccessTokenRepositoryStub
@@ -23,15 +42,27 @@ const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
 
   return new LoadAccountByAccessTokenRepositoryStub()
 }
+
+const makeGetBuyBooksRepositoryStub = (): GetBuyBooksRepository => {
+  class GetBuyBooksRepositoryStub implements GetBuyBooksRepository {
+    async getBuyBooks(userId: string): Promise<AddBuyBookModel[] | null> {
+      return await Promise.resolve([makeFakeAddBuyBook(), makeFakeAddBuyBook()])
+    }
+  }
+  return new GetBuyBooksRepositoryStub()
+}
 interface SutTypes {
+  getBuyBooksRepositoryStub: GetBuyBooksRepository
   loadAccountStub: LoadAccountByAccessTokenRepository
   sut: DbGetBuyBooks
 }
 
 const makeSut = (): SutTypes => {
+  const getBuyBooksRepositoryStub = makeGetBuyBooksRepositoryStub()
   const loadAccountStub = makeLoadAccountStub()
-  const sut = new DbGetBuyBooks(loadAccountStub)
+  const sut = new DbGetBuyBooks(loadAccountStub, getBuyBooksRepositoryStub)
   return {
+    getBuyBooksRepositoryStub,
     loadAccountStub,
     sut,
   }
@@ -61,5 +92,12 @@ describe('DbGetBuyBooks', () => {
       .mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.getBuyBooks('any_token')
     expect(response).toBeFalsy()
+  })
+
+  test('should call getBooks with correct accessToken', async () => {
+    const { sut, getBuyBooksRepositoryStub } = makeSut()
+    const getSpy = jest.spyOn(getBuyBooksRepositoryStub, 'getBuyBooks')
+    await sut.getBuyBooks('any_token')
+    expect(getSpy).toHaveBeenCalledWith('any_id')
   })
 })
