6eb8dee9529ddce234ace705c912172daae37dd7
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 19:06:17 -0300

    test: ensure DbGetBuyBooks return null if loadAccount return null

--- src/server/data/usecases/book-buy-list/get-buy-books-list/db-get-buy-books-list.ts
+++ src/server/data/usecases/book-buy-list/get-buy-books-list/db-get-buy-books-list.ts
@@ -1,13 +1,17 @@
 import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
 import { GetBuyBooks } from '../../../../domain/usecases/book-buy-list/get-books-buy-list'
 import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { GetBuyBooksRepository } from '../../../protocols/db/book-buy-list/get-books-buy-list-repository'
 
 export class DbGetBuyBooks implements GetBuyBooks {
   constructor(
     private readonly loadAccount: LoadAccountByAccessTokenRepository
   ) {}
   async getBuyBooks(accessToken: string): Promise<AddBuyBookModel[] | null> {
-    await this.loadAccount.loadByAccessToken(accessToken)
-    return null
+    const account = await this.loadAccount.loadByAccessToken(accessToken)
+    if (!account) {
+      return null
+    }
+    return []
   }
 }
