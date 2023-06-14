cc4943eafb276e3879944cdd97d0314549a0235b
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 08:17:47 -0300

    feat: ensure RemoveAmountBuyBookListController call RemoveBookAmount with correct values

--- src/server/presentation/controllers/book-buy-list/remove-book-buy-list/remove-book-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/remove-book-buy-list/remove-book-buy-list-controller.ts
@@ -1,15 +1,21 @@
+import { RemoveAmountBuyBook } from '../../../../domain/usecases/book-buy-list/remove-amount-book-buy-list'
 import { badRequest, ok } from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 
-export class RemoveBookBuyListController implements Controller {
-  constructor(private readonly validator: Validation) {}
+export class RemoveAmountBuyBookListController implements Controller {
+  constructor(
+    private readonly validator: Validation,
+    private readonly removeBookAmount: RemoveAmountBuyBook
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken, bookId } = httpRequest.body
+    await this.removeBookAmount.removeAmount(accessToken, bookId)
     return await Promise.resolve(ok('success'))
   }
 }
