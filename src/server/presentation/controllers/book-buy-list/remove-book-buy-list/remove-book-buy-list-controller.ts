3ad0f56503daca61f490a1b77c794ddabdf2dc27
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 08:45:56 -0300

    feat: ensure RemoveAmountBuyBookListController return 500 if RemoveBookAmount fails

--- src/server/presentation/controllers/book-buy-list/remove-book-buy-list/remove-book-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/remove-book-buy-list/remove-book-buy-list-controller.ts
@@ -1,5 +1,10 @@
 import { RemoveAmountBuyBook } from '../../../../domain/usecases/book-buy-list/remove-amount-book-buy-list'
-import { badRequest, ok, unauthorized } from '../../../helpers/http'
+import {
+  badRequest,
+  ok,
+  serverError,
+  unauthorized,
+} from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
@@ -10,18 +15,22 @@ export class RemoveAmountBuyBookListController implements Controller {
     private readonly removeBookAmount: RemoveAmountBuyBook
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validator.validation(httpRequest)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validator.validation(httpRequest)
+      if (error) {
+        return badRequest(error)
+      }
+      const { accessToken, bookId } = httpRequest.body
+      const removeBook = await this.removeBookAmount.removeAmount(
+        accessToken,
+        bookId
+      )
+      if (!removeBook) {
+        return unauthorized()
+      }
+      return await Promise.resolve(ok('success'))
+    } catch (err) {
+      return serverError(err as Error)
     }
-    const { accessToken, bookId } = httpRequest.body
-    const removeBook = await this.removeBookAmount.removeAmount(
-      accessToken,
-      bookId
-    )
-    if (!removeBook) {
-      return unauthorized()
-    }
-    return await Promise.resolve(ok('success'))
   }
 }
