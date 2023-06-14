6a9f1687b425a2efc25b5eff8502707bd43e2dd8
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 10:59:11 -0300

    test: ensure RemoveAmountBuyBookValidator return MissingParamsError if accessToken not provided

---
+++ src/server/presentation/validators/book-buy-list/remove-book-amount-buy-list-validator.spec.ts
@@ -0,0 +1,22 @@
+import { MissingParamError } from '../../errors/missing-params-error'
+import { HttpRequest } from '../../protocols/http'
+import { RemoveAmountBuyBookValidator } from './remove-book-amount-buy-list-validator'
+const makeFakeRequest = (field: string): HttpRequest => {
+  const body: any = {
+    accessToken: 'any_token',
+    bookId: 'any_book_id',
+  }
+  delete body[field]
+  return {
+    body,
+  }
+}
+const makeSut = (): RemoveAmountBuyBookValidator =>
+  new RemoveAmountBuyBookValidator()
+describe('RemoveAmountBuyBookValidator', () => {
+  test('should return MissingParamsError if accessToken not provided', async () => {
+    const sut = makeSut()
+    const response = sut.validation(makeFakeRequest('accessToken'))
+    expect(response).toEqual(new MissingParamError('accessToken'))
+  })
+})
