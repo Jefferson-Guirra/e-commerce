92bd0e53e08016409e965e0496ec5334593b800e
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 18:12:25 -0300

    test: ensure GetBuyBooksController return 400 if accessToken not provided

--- src/server/presentation/controllers/book-buy-list/get-books-buy-list/get-books-buy-list-controller.spec.ts
+++ src/server/presentation/controllers/book-buy-list/get-books-buy-list/get-books-buy-list-controller.spec.ts
@@ -1,6 +1,8 @@
+import { MissingParamError } from '../../../errors/missing-params-error'
+import { badRequest } from '../../../helpers/http'
 import { HttpRequest } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
-import { GetBuyBooksController } from './get-books-buy-list'
+import { GetBuyBooksController } from './get-books-buy-list-controller'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -35,5 +37,14 @@ describe('GetBuyBooksController', () => {
     const validationSpy = jest.spyOn(validatorStub, 'validation')
     await sut.handle(makeFakeRequest())
     expect(validationSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+
+  test('should return 400 if validation return a error', async () => {
+    const { sut, validatorStub } = makeSut()
+    jest
+      .spyOn(validatorStub, 'validation')
+      .mockReturnValue(new MissingParamError('any_field'))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
