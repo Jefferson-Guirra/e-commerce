01f4297573d15b6a53614843c945edadf9851e41
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-14 08:18:52 -0300

    test: ensure RemoveAmountBuyBookListController call RemoveBookAmount with correct values

--- src/server/presentation/controllers/book-buy-list/remove-book-buy-list/remove-book-buy-list-controller.spec.ts
+++ src/server/presentation/controllers/book-buy-list/remove-book-buy-list/remove-book-buy-list-controller.spec.ts
@@ -1,8 +1,27 @@
+import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
+import { RemoveAmountBuyBook } from '../../../../domain/usecases/book-buy-list/remove-amount-book-buy-list'
 import { MissingParamError } from '../../../errors/missing-params-error'
 import { badRequest, ok } from '../../../helpers/http'
 import { HttpRequest } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
-import { RemoveBookBuyListController } from './remove-book-buy-list-controller'
+import { RemoveAmountBuyBookListController } from './remove-book-buy-list-controller'
+
+const makeFakeAddBuyBook = (): AddBuyBookModel => ({
+  authors: ['any_author'],
+  amount: 0,
+  date: 0,
+  description: 'any_description',
+  title: 'any_title',
+  bookId: 'any_book_id',
+  id: 'any_id',
+  imgUrl: 'any_url',
+  language: 'any-language',
+  price: 0,
+  publisher: 'any_publisher',
+  publisherDate: 'any_date',
+  queryDoc: 'any_id_doc',
+  userId: 'any_user_id',
+})
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -19,15 +38,33 @@ const makeValidateStub = (): Validation => {
   }
   return new ValidationStub()
 }
+
+const makeRemoveAmountBuyBookStub = (): RemoveAmountBuyBook => {
+  class RemoveAmountBuyBookStub implements RemoveAmountBuyBook {
+    async removeAmount(
+      accessToken: string,
+      bookId: string
+    ): Promise<AddBuyBookModel | undefined> {
+      return await Promise.resolve(makeFakeAddBuyBook())
+    }
+  }
+  return new RemoveAmountBuyBookStub()
+}
 interface SutTypes {
+  removeAmountStub: RemoveAmountBuyBook
   validateStub: Validation
-  sut: RemoveBookBuyListController
+  sut: RemoveAmountBuyBookListController
 }
 
 const makeSut = (): SutTypes => {
+  const removeAmountStub = makeRemoveAmountBuyBookStub()
   const validateStub = makeValidateStub()
-  const sut = new RemoveBookBuyListController(validateStub)
+  const sut = new RemoveAmountBuyBookListController(
+    validateStub,
+    removeAmountStub
+  )
   return {
+    removeAmountStub,
     validateStub,
     sut,
   }
@@ -48,5 +85,12 @@ describe('RemoveBookBuyListController', () => {
       .mockReturnValue(new MissingParamError('any_field'))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('should call removeBookAmount with correct values', async () => {
+    const { sut, removeAmountStub } = makeSut()
+    const removeAmountSpy = jest.spyOn(removeAmountStub, 'removeAmount')
+    await sut.handle(makeFakeRequest())
+    expect(removeAmountSpy).toHaveBeenCalledWith('any_token', 'any_book_id')
   })
 })
