e82c52f39b4cb85f4edd11daa6cc08446f3d7831
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-15 10:28:38 -0300

    test: ensure DeleteBuyBookListController return 500 if DeleteBuyBookList fails

--- src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
+++ src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
@@ -1,7 +1,7 @@
 import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
 import { DeleteBuyBookList } from '../../../../domain/usecases/book-buy-list/delete-book-buy-list'
 import { MissingParamError } from '../../../errors/missing-params-error'
-import { badRequest, unauthorized } from '../../../helpers/http'
+import { badRequest, serverError, unauthorized } from '../../../helpers/http'
 import { HttpRequest } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 import { DeleteBuyBookListController } from './delete-book-buy-list-controller'
@@ -96,5 +96,14 @@ describe('DeleteBuyBookList', () => {
       .mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(unauthorized())
+  })
+
+  test('should return 500 if deleteBook fails ', async () => {
+    const { sut, deleteBuyBookStub } = makeSut()
+    jest
+      .spyOn(deleteBuyBookStub, 'deleteBook')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(serverError())
   })
 })
