0869cc8676fce8e36bcaaa27bb1f1a3e014e7125
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-15 10:33:00 -0300

    test: ensure DeleteBuyBookListController return a book on succeeds

--- src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
+++ src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
@@ -1,7 +1,12 @@
 import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
 import { DeleteBuyBookList } from '../../../../domain/usecases/book-buy-list/delete-book-buy-list'
 import { MissingParamError } from '../../../errors/missing-params-error'
-import { badRequest, serverError, unauthorized } from '../../../helpers/http'
+import {
+  badRequest,
+  ok,
+  serverError,
+  unauthorized,
+} from '../../../helpers/http'
 import { HttpRequest } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 import { DeleteBuyBookListController } from './delete-book-buy-list-controller'
@@ -105,5 +110,11 @@ describe('DeleteBuyBookList', () => {
       .mockReturnValueOnce(Promise.reject(new Error()))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(serverError())
+  })
+
+  test('should return 200 on succeeds', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(ok(makeFakeAddBuyBook()))
   })
 })
