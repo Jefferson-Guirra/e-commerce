778d0bf98ff67f80317dbcd3de20c50be170effb
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-15 12:07:50 -0300

    feat: create DeleteBuyBookListController to a factory method

---
+++ src/server/main/factories/book-buy-list/delete-book-buy-list-controller-factory.ts
@@ -0,0 +1,17 @@
+import { DbDeleteBuyBookList } from '../../../data/usecases/book-buy-list/delete-buy-book-list/db-delete-buy-book-list'
+import { AccountMongoRepository } from '../../../infra/db/account/account-mongo-repository'
+import { BuyBooksListMongoRepository } from '../../../infra/db/book-buy-list/book-buy-list-mongo-repository'
+import { DeleteBuyBookListController } from '../../../presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list-controller'
+import { Controller } from '../../../presentation/protocols/controller'
+import { DeleteBuyBookListValidator } from '../../../presentation/validators/book-buy-list/delete-buy-book-list-validator'
+
+export const makeDeleteBuyBookListController = (): Controller => {
+  const validator = new DeleteBuyBookListValidator()
+  const accountMongoRepository = new AccountMongoRepository()
+  const buyBooksListMongoRepository = new BuyBooksListMongoRepository()
+  const dbDeleteBuyBookList = new DbDeleteBuyBookList(
+    accountMongoRepository,
+    buyBooksListMongoRepository
+  )
+  return new DeleteBuyBookListController(validator, dbDeleteBuyBookList)
+}
