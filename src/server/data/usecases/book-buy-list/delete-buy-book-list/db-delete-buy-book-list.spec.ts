3e3cf61d385d9ebbffba2aea7cf8d2d5bfab6122
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-15 10:52:05 -0300

    test: ensure DbDeleteBuyBookList call LoadAccount with correct token

---
+++ src/server/data/usecases/book-buy-list/delete-buy-book-list/db-delete-buy-book-list.spec.ts
@@ -0,0 +1,45 @@
+import {
+  LoadAccountByAccessTokenRepository,
+  accountLoginModel,
+} from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { DbDeleteBuyBookList } from './db-delete-buy-book-list'
+
+const makeLoadAccountByAccessTokenStun =
+  (): LoadAccountByAccessTokenRepository => {
+    class LoadAccountBysAccessTokenRepositoryStub
+      implements LoadAccountByAccessTokenRepository
+    {
+      async loadByAccessToken(
+        accessToken: string
+      ): Promise<accountLoginModel | null> {
+        return await Promise.resolve({
+          username: 'any_username',
+          password: 'any_password',
+          email: 'any_email@mail.com',
+          id: 'any_id',
+          accessToken: 'any_token',
+        })
+      }
+    }
+    return new LoadAccountBysAccessTokenRepositoryStub()
+  }
+interface SutTypes {
+  loadAccountStub: LoadAccountByAccessTokenRepository
+  sut: DbDeleteBuyBookList
+}
+const makeSut = (): SutTypes => {
+  const loadAccountStub = makeLoadAccountByAccessTokenStun()
+  const sut = new DbDeleteBuyBookList(loadAccountStub)
+  return {
+    loadAccountStub,
+    sut,
+  }
+}
+describe('first', () => {
+  test('should call loadAccount with correct token', async () => {
+    const { sut, loadAccountStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountStub, 'loadByAccessToken')
+    await sut.deleteBook('any_token', 'any_id')
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
+})
