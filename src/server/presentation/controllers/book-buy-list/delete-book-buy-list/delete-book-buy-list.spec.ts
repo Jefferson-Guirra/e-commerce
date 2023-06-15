877282e12e1cae20e2dce54a0b09c8bab4f85481
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-15 10:05:04 -0300

    test: ensure DeleteBuyBookListController call Validation with correct values

---
+++ src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
@@ -0,0 +1,39 @@
+import { HttpRequest } from '../../../protocols/http'
+import { Validation } from '../../../protocols/validate'
+import { DeleteBuyBookListController } from './delete-book-buy-list-controller'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    accessToken: 'any_token',
+    bookId: 'any_id',
+  },
+})
+
+const makeValidatorStub = (): Validation => {
+  class ValidatorStub implements Validation {
+    validation(input: any): Error | undefined {
+      return
+    }
+  }
+  return new ValidatorStub()
+}
+interface SutTypes {
+  validatorStub: Validation
+  sut: DeleteBuyBookListController
+}
+const makeSut = (): SutTypes => {
+  const validatorStub = makeValidatorStub()
+  const sut = new DeleteBuyBookListController(validatorStub)
+  return {
+    validatorStub,
+    sut,
+  }
+}
+describe('DeleteBuyBookList', () => {
+  test('should call validation wit correct values', async () => {
+    const { sut, validatorStub } = makeSut()
+    const validationSpy = jest.spyOn(validatorStub, 'validation')
+    await sut.handle(makeFakeRequest())
+    expect(validationSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
