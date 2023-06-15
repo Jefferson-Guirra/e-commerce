66ffea6091b111af17bb58ab9d6d18501e354056
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-15 10:19:34 -0300

    test: ensure DeleteBuyBookListController call DeleteBuyBookList with correct values

--- src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
+++ src/server/presentation/controllers/book-buy-list/delete-book-buy-list/delete-book-buy-list.spec.ts
@@ -1,3 +1,5 @@
+import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
+import { DeleteBuyBookList } from '../../../../domain/usecases/book-buy-list/delete-book-buy-list'
 import { MissingParamError } from '../../../errors/missing-params-error'
 import { badRequest } from '../../../helpers/http'
 import { HttpRequest } from '../../../protocols/http'
@@ -11,6 +13,23 @@ const makeFakeRequest = (): HttpRequest => ({
   },
 })
 
+const makeFakeAddBuyBook = (): AddBuyBookModel => ({
+  authors: ['any_author'],
+  amount: 0,
+  date: 0,
+  description: 'any_description',
+  title: 'any_title',
+  bookId: 'any_book_id',
+  id: 'any_id',
+  imgUrl: 'any_url',
+  language: 'any-language',
+  price: 0,
+  publisher: 'any_publisher',
+  publisherDate: 'any_date',
+  queryDoc: 'any_id_doc',
+  userId: 'any_user_id',
+})
+
 const makeValidatorStub = (): Validation => {
   class ValidatorStub implements Validation {
     validation(input: any): Error | undefined {
@@ -19,14 +38,29 @@ const makeValidatorStub = (): Validation => {
   }
   return new ValidatorStub()
 }
+
+const makeDeleteBuyBookListStub = (): DeleteBuyBookList => {
+  class DeleteBuyBookListStub implements DeleteBuyBookList {
+    async deleteBook(
+      accessToken: string,
+      bookId: string
+    ): Promise<AddBuyBookModel | null> {
+      return await Promise.resolve(makeFakeAddBuyBook())
+    }
+  }
+  return new DeleteBuyBookListStub()
+}
 interface SutTypes {
   validatorStub: Validation
+  deleteBuyBookStub: DeleteBuyBookList
   sut: DeleteBuyBookListController
 }
 const makeSut = (): SutTypes => {
+  const deleteBuyBookStub = makeDeleteBuyBookListStub()
   const validatorStub = makeValidatorStub()
-  const sut = new DeleteBuyBookListController(validatorStub)
+  const sut = new DeleteBuyBookListController(validatorStub, deleteBuyBookStub)
   return {
+    deleteBuyBookStub,
     validatorStub,
     sut,
   }
@@ -46,5 +80,12 @@ describe('DeleteBuyBookList', () => {
       .mockReturnValueOnce(new MissingParamError('any_field'))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('should call deleBuyBook wit correct values', async () => {
+    const { sut, deleteBuyBookStub } = makeSut()
+    const deleteSpy = jest.spyOn(deleteBuyBookStub, 'deleteBook')
+    await sut.handle(makeFakeRequest())
+    expect(deleteSpy).toHaveBeenCalledWith('any_token', 'any_id')
   })
 })
