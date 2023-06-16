98efd3dc272ce1ca4802896b7b6980b79c478351
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 08:55:14 -0300

    feat: ensure GetBookListController call FindBook with correct values

--- src/server/presentation/controllers/book-list/get-book-list/get-book-list-controller.ts
+++ src/server/presentation/controllers/book-list/get-book-list/get-book-list-controller.ts
@@ -1,15 +1,21 @@
+import { GetBookList } from '../../../../domain/usecases/book-list/get-book-list'
 import { badRequest, ok } from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 
 export class GetBookListController implements Controller {
-  constructor(private readonly validator: Validation) {}
+  constructor(
+    private readonly validator: Validation,
+    private readonly findBook: GetBookList
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken, bookId } = httpRequest.body
+    await this.findBook.getBook(accessToken, bookId)
     return ok('success')
   }
 }
