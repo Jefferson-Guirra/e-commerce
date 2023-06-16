69db13dd5090f38b8bdf395eea20197526487442
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 09:53:02 -0300

    test: ensure GetBookListValidator return MissingParamsError if accessToken not provided

---
+++ src/server/presentation/validators/book-list/get-book-list.spec.ts
@@ -0,0 +1,22 @@
+import { MissingParamError } from '../../errors/missing-params-error'
+import { HttpRequest } from '../../protocols/http'
+import { GetBookListValidator } from './get-book-list-validator'
+
+const makeFakeRequest = (field: string): HttpRequest => {
+  const body: any = {
+    accessToken: 'any_token',
+    bookId: 'any_book_id',
+  }
+  delete body[field]
+  return {
+    body,
+  }
+}
+const makeSut = (): GetBookListValidator => new GetBookListValidator()
+describe('GetBookListValidator', () => {
+  test('should return MissingParamsError if accessToken not provided', async () => {
+    const sut = makeSut()
+    const response = sut.validation(makeFakeRequest('accessToken'))
+    expect(response).toEqual(new MissingParamError('accessToken'))
+  })
+})
