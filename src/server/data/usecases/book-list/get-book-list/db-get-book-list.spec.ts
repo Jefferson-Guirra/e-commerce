868c62620c436e01b214da3987986ccdab142d7b
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 09:18:04 -0300

    test: ensure DbGetBookList call LoadAccount with correct token

---
+++ src/server/data/usecases/book-list/get-book-list/db-get-book-list.spec.ts
@@ -0,0 +1,45 @@
+import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { accountLoginModel } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { DbGetBookList } from './db-get-book-list'
+
+const makeLoadAccountByAccessToken = (): LoadAccountByAccessTokenRepository => {
+  class LoadAccountByAccessTokenStub
+    implements LoadAccountByAccessTokenRepository
+  {
+    async loadByAccessToken(
+      accessToken: string
+    ): Promise<accountLoginModel | null> {
+      return await Promise.resolve({
+        username: 'any_username',
+        password: 'any_password',
+        email: 'any_email',
+        accessToken: 'any_token',
+        id: 'any_user_id',
+      })
+    }
+  }
+  return new LoadAccountByAccessTokenStub()
+}
+
+interface SutTypes {
+  loadAccountRepositoryStub: LoadAccountByAccessTokenRepository
+  sut: DbGetBookList
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountRepositoryStub = makeLoadAccountByAccessToken()
+  const sut = new DbGetBookList(loadAccountRepositoryStub)
+  return {
+    loadAccountRepositoryStub,
+    sut,
+  }
+}
+
+describe('DbGetBookList', () => {
+  test('should call loadAccount with correct value', async () => {
+    const { sut, loadAccountRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountRepositoryStub, 'loadByAccessToken')
+    await sut.getBook('any_token', 'any_id')
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
+})
