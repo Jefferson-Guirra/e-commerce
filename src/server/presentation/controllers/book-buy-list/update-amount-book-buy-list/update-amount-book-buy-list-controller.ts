f35ec3fdf792c0c8ed24d9d0a23e7cebdf315a69
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 20:37:18 -0300

    feat: esure UpdateAmountBookBuyListController call UpdateBuyBook with correct values

--- src/server/presentation/controllers/book-buy-list/update-amount-book-buy-list/update-amount-book-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/update-amount-book-buy-list/update-amount-book-buy-list-controller.ts
@@ -1,15 +1,21 @@
+import { UpdateAmountBuyBook } from '../../../../domain/usecases/book-buy-list/update-amount-book-buy-list'
 import { badRequest, ok } from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 
 export class UpdateAmountBookBuyListController implements Controller {
-  constructor(private readonly validator: Validation) {}
+  constructor(
+    private readonly validator: Validation,
+    private readonly updateBuyBook: UpdateAmountBuyBook
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken, bookId } = httpRequest.body
+    await this.updateBuyBook.updateAmount(accessToken, bookId)
     return ok('success')
   }
 }
