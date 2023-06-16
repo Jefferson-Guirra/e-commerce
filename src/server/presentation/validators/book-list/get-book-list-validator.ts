4568d0df8d26a2cc994a8f781232c17cfd09d5f6
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 09:55:30 -0300

    test: ensure GetBookListValidator return MissingParamsError if bookId  not provided

--- src/server/presentation/validators/book-list/get-book-list-validator.ts
+++ src/server/presentation/validators/book-list/get-book-list-validator.ts
@@ -2,7 +2,7 @@ import { Validation } from '../../protocols/validate'
 import { MissingParamError } from '../../errors/missing-params-error'
 export class GetBookListValidator implements Validation {
   validation(input: any): Error | undefined {
-    const requiredFields = ['accessToken']
+    const requiredFields = ['accessToken', 'bookId']
     for (const field of requiredFields) {
       if (!input.body[field]) {
         return new MissingParamError(field)
