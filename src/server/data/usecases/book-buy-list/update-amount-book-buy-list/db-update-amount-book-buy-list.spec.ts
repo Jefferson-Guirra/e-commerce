9c47c277f758f53793008644a62c93fcb73cae0b
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 21:27:26 -0300

    test: ensure DbUpdateAmountBookBuyList call LoadBook with correct values

--- src/server/data/usecases/book-buy-list/update-amount-book-buy-list/db-update-amount-book-buy-list.spec.ts
+++ src/server/data/usecases/book-buy-list/update-amount-book-buy-list/db-update-amount-book-buy-list.spec.ts
@@ -1,6 +1,40 @@
 import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
 import { accountLoginModel } from '../../../protocols/db/account/load-account-by-access-token-repository'
 import { DbUpdateAmountBookBuyList } from './db-update-amount-book-buy-list'
+import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
+import { LoadBuyBookByQueryDocRepository } from '../../../protocols/db/book-buy-list/load-book-buy-list-by-query-doc-repository'
+
+const makeFakeAddBuyBook = (): AddBuyBookModel => ({
+  authors: ['any_author'],
+  description: 'any_description',
+  title: 'any_title',
+  imgUrl: 'any_url',
+  bookId: 'any_book_id',
+  language: 'any_language',
+  price: 0,
+  publisher: 'any_publisher',
+  publisherDate: 'any_date',
+  amount: 1,
+  date: 0,
+  id: 'any_id',
+  userId: 'any_user_id',
+  queryDoc: 'any_user_id' + 'any_id',
+})
+
+const makeLoadBookByQueryDocStub = (): LoadBuyBookByQueryDocRepository => {
+  class LoadBookByQueryDocRepositoryStub
+    implements LoadBuyBookByQueryDocRepository
+  {
+    async loadBookByQueryDoc(
+      userId: string,
+      bookId: string
+    ): Promise<AddBuyBookModel | null> {
+      return await Promise.resolve(makeFakeAddBuyBook())
+    }
+  }
+
+  return new LoadBookByQueryDocRepositoryStub()
+}
 
 const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
   class LoadAccountByAccessTokenRepositoryStub
@@ -13,7 +47,7 @@ const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
         username: 'any_username',
         password: 'any_password',
         email: 'any_email@mail.com',
-        id: 'any_id',
+        id: 'any_user_id',
         accessToken: 'any_token',
       })
     }
@@ -22,14 +56,20 @@ const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
 }
 
 interface SutTypes {
+  loadBookByQueryDocRepositoryStub: LoadBuyBookByQueryDocRepository
   loadAccountStub: LoadAccountByAccessTokenRepository
   sut: DbUpdateAmountBookBuyList
 }
 
 const makeSut = (): SutTypes => {
+  const loadBookByQueryDocRepositoryStub = makeLoadBookByQueryDocStub()
   const loadAccountStub = makeLoadAccountStub()
-  const sut = new DbUpdateAmountBookBuyList(loadAccountStub)
+  const sut = new DbUpdateAmountBookBuyList(
+    loadAccountStub,
+    loadBookByQueryDocRepositoryStub
+  )
   return {
+    loadBookByQueryDocRepositoryStub,
     loadAccountStub,
     sut,
   }
@@ -59,5 +99,15 @@ describe('DbUpdateAmountBookBuyList', () => {
       .mockReturnValueOnce(Promise.resolve(null))
     const response = await sut.updateAmount('any_token', 'any_book_id', 1)
     expect(response).toBeFalsy()
+  })
+
+  test('should call LoadBookByQueryDoc with correct values', async () => {
+    const { sut, loadBookByQueryDocRepositoryStub } = makeSut()
+    const loadBookSpy = jest.spyOn(
+      loadBookByQueryDocRepositoryStub,
+      'loadBookByQueryDoc'
+    )
+    await sut.updateAmount('any_token', 'any_book_id', 1)
+    expect(loadBookSpy).toBeCalledWith('any_user_id', 'any_book_id')
   })
 })
