159c272171b0fa25b8069fd86a95d89ff6a97abe
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 21:45:06 -0300

    feat: ensure DbUpdateAmountBookBuyList call addAmountBuyBook with correct values

--- src/server/data/usecases/book-buy-list/update-amount-book-buy-list/db-update-amount-book-buy-list.ts
+++ src/server/data/usecases/book-buy-list/update-amount-book-buy-list/db-update-amount-book-buy-list.ts
@@ -1,12 +1,14 @@
 import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
 import { UpdateAmountBuyBook } from '../../../../domain/usecases/book-buy-list/update-amount-book-buy-list'
 import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { AddAmountBuyBookRepository } from '../../../protocols/db/book-buy-list/add-amount-book-buy-list-repository'
 import { LoadBuyBookByQueryDocRepository } from '../../../protocols/db/book-buy-list/load-book-buy-list-by-query-doc-repository'
 
 export class DbUpdateAmountBookBuyList implements UpdateAmountBuyBook {
   constructor(
     private readonly loadAccount: LoadAccountByAccessTokenRepository,
-    private readonly loadBook: LoadBuyBookByQueryDocRepository
+    private readonly loadBook: LoadBuyBookByQueryDocRepository,
+    private readonly addAmountBuyBook: AddAmountBuyBookRepository
   ) {}
   async updateAmount(
     accessToken: string,
@@ -24,6 +26,8 @@ export class DbUpdateAmountBookBuyList implements UpdateAmountBuyBook {
     if (!book) {
       return null
     }
+
+    await this.addAmountBuyBook.addAmount(book, amount)
     return {
       authors: ['any_author'],
       amount: 0,
