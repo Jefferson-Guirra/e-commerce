cfa7a14d9b034f6aee0c312edd0171b1aeb39e91
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 21:11:41 -0300

    test: ensure DbUpdateAmountBookBuyList call LoadAccount with correct token

---
+++ src/server/data/usecases/book-buy-list/update-amount-book-buy-list/db-update-amount-book-buy-list.spec.ts
@@ -0,0 +1,45 @@
+import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { accountLoginModel } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { DbUpdateAmountBookBuyList } from './db-update-amount-book-buy-list'
+
+const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
+  class LoadAccountByAccessTokenRepositoryStub
+    implements LoadAccountByAccessTokenRepository
+  {
+    async loadByAccessToken(
+      accessToken: string
+    ): Promise<accountLoginModel | null> {
+      return await Promise.resolve({
+        username: 'any_username',
+        password: 'any_password',
+        email: 'any_email@mail.com',
+        id: 'any_id',
+        accessToken: 'any_token',
+      })
+    }
+  }
+  return new LoadAccountByAccessTokenRepositoryStub()
+}
+
+interface SutTypes {
+  loadAccountStub: LoadAccountByAccessTokenRepository
+  sut: DbUpdateAmountBookBuyList
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountStub = makeLoadAccountStub()
+  const sut = new DbUpdateAmountBookBuyList(loadAccountStub)
+  return {
+    loadAccountStub,
+    sut,
+  }
+}
+
+describe('DbUpdateAmountBookBuyList', () => {
+  test('should call loadAccountByAccessToken wit correct token', async () => {
+    const { sut, loadAccountStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountStub, 'loadByAccessToken')
+    await sut.updateAmount('any_token', 'any_book_id', 1)
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
+  })
+})
