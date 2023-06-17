030437f0d4600eb6ad52faa41ae73489d6177440
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-16 22:57:00 -0300

    feat: create update-amount buy book list route

---
+++ src/pages/api/buybooklist/update-amount.ts
@@ -0,0 +1,4 @@
+import { adapterRouter } from '../../../server/main/adapters/next/adapter-route'
+import { makeUpdateAmountBookBuyListController } from '../../../server/main/factories/book-buy-list/update-amount-book-buy-list-controller-factory'
+
+export default adapterRouter(makeUpdateAmountBookBuyListController())
