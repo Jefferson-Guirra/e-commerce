a54b6b419dd334becc550a99703ae9315dfd8c64
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-18 10:19:10 -0300

    test: ensure DeleteAllBuyBooKListController call DeleteAllBooks with correct values

--- src/server/presentation/controllers/book-buy-list/delete-all-book-buy-list/delete-all-book-buy-list-controller.spec.ts
+++ src/server/presentation/controllers/book-buy-list/delete-all-book-buy-list/delete-all-book-buy-list-controller.spec.ts
@@ -3,6 +3,7 @@ import { Validation } from '../../../protocols/validate'
 import { DeleteAllBuyBooKListController } from './delete-all-book-buy-list-controller'
 import { badRequest } from '../../../helpers/http'
 import { MissingParamError } from '../../../errors/missing-params-error'
+import { DeleteAllBuyBookList } from '../../../../domain/usecases/book-buy-list/delete-all-book-buy-list'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -19,15 +20,30 @@ const makeValidatorStub = (): Validation => {
   return new ValidatorStub()
 }
 
+const makeDeleteAllBuyBooksStub = (): DeleteAllBuyBookList => {
+  class DeleteAllBuyBookListStub implements DeleteAllBuyBookList {
+    async deleteAllBooks(accessToken: string): Promise<void | null> {
+      await Promise.resolve()
+    }
+  }
+  return new DeleteAllBuyBookListStub()
+}
+
 interface SutTypes {
+  deleteAllBuyBooksStub: DeleteAllBuyBookList
   validatorStub: Validation
   sut: DeleteAllBuyBooKListController
 }
 
 const makeSut = (): SutTypes => {
+  const deleteAllBuyBooksStub = makeDeleteAllBuyBooksStub()
   const validatorStub = makeValidatorStub()
-  const sut = new DeleteAllBuyBooKListController(validatorStub)
+  const sut = new DeleteAllBuyBooKListController(
+    validatorStub,
+    deleteAllBuyBooksStub
+  )
   return {
+    deleteAllBuyBooksStub,
     validatorStub,
     sut,
   }
@@ -48,5 +64,12 @@ describe('DeleteAllBuyBooKListController', () => {
       .mockReturnValueOnce(new MissingParamError('any_field'))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('should call deleteAllBooks wit correct values', async () => {
+    const { sut, deleteAllBuyBooksStub } = makeSut()
+    const deleteSpy = jest.spyOn(deleteAllBuyBooksStub, 'deleteAllBooks')
+    await sut.handle(makeFakeRequest())
+    expect(deleteSpy).toHaveBeenCalledWith('any_token')
   })
 })
