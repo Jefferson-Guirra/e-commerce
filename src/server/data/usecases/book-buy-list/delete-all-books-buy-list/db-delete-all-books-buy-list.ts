00aa41789fd5ab10763006bcb8030a83a79647a5
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-18 13:50:49 -0300

    feat: ensure DbDeleteAllBooksBuyList call DeleteAllBuyBooks with correct id

--- src/server/data/usecases/book-buy-list/delete-all-books-buy-list/db-delete-all-books-buy-list.ts
+++ src/server/data/usecases/book-buy-list/delete-all-books-buy-list/db-delete-all-books-buy-list.ts
@@ -1,9 +1,11 @@
 import { DeleteAllBuyBookList } from '../../../../domain/usecases/book-buy-list/delete-all-book-buy-list'
 import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+import { DeleteAllBuyBooksListRepository } from '../../../protocols/db/book-buy-list/delete-all-books-buy-list-repository'
 
 export class DbDeleteAllBooksBuyList implements DeleteAllBuyBookList {
   constructor(
-    private readonly loadAccount: LoadAccountByAccessTokenRepository
+    private readonly loadAccount: LoadAccountByAccessTokenRepository,
+    private readonly deleteAllBuyBooks: DeleteAllBuyBooksListRepository
   ) {}
 
   async deleteAllBooks(accessToken: string): Promise<void | null> {
@@ -11,5 +13,8 @@ export class DbDeleteAllBooksBuyList implements DeleteAllBuyBookList {
     if (!account) {
       return null
     }
+
+    const { id } = account
+    await this.deleteAllBuyBooks.deleteAllBooks(id)
   }
 }
