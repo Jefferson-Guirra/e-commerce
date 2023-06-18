65405d7dd3f9cd030e84ca72e18a37074b5a7916
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-18 10:17:57 -0300

    feat: ensure DeleteAllBuyBooKListController call DeleteAllBooks with correct values

--- src/server/presentation/controllers/book-buy-list/delete-all-book-buy-list/delete-all-book-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/delete-all-book-buy-list/delete-all-book-buy-list-controller.ts
@@ -1,15 +1,21 @@
+import { DeleteAllBuyBookList } from '../../../../domain/usecases/book-buy-list/delete-all-book-buy-list'
 import { badRequest, ok } from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 
 export class DeleteAllBuyBooKListController implements Controller {
-  constructor(private readonly validator: Validation) {}
+  constructor(
+    private readonly validator: Validation,
+    private readonly deleteAllBuyBooks: DeleteAllBuyBookList
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken } = httpRequest.body
+    await this.deleteAllBuyBooks.deleteAllBooks(accessToken)
     return ok('success')
   }
 }
