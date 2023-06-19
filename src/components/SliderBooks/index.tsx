a630d7a6823f186611cfdcb3edffffcd8b6ac76d
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-19 15:54:36 -0300

    refactor: config next Image

--- src/components/SliderBooks/index.tsx
+++ src/components/SliderBooks/index.tsx
@@ -39,15 +39,15 @@ const SliderBooks = ({ bookList }: Props) => {
             className={styles.container}
             onClick={() => router.push(`/Book/${item.id}`)}
           >
-            <div
-              style={{ width: '100%', height: '250px', position: 'relative' }}
-            >
-              <Image
-                fill
-                src={`https://books.google.com/books/publisher/content/images/frontcover/${item.id}?fife=w340-h300&source=gbs_api`}
-                alt={`Imagem do Livro ${item.title}`}
-              />
-            </div>
+            <Image
+              priority
+              style={{ width: '100%', height: '250px' }}
+              width={200}
+              height={0}
+              src={`https://books.google.com/books/publisher/content/images/frontcover/${item.id}?fife=w340-h300&source=gbs_api`}
+              alt={`Imagem do Livro ${item.title}`}
+            />
+
             <div className={styles.info}>
               <div className={styles.titleBook}>
                 <p>{item.title}</p>
