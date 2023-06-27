f5aadbba9c192cbd24dd269226993408169619ea
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-27 16:33:32 -0300

    test: ensure DebGetBookBuyList call GetBook with correct values

--- src/server/data/usecases/book-buy-list/get-book-buy-list/db-get-book-list.spec.ts
+++ src/server/data/usecases/book-buy-list/get-book-buy-list/db-get-book-list.spec.ts
@@ -3,6 +3,38 @@ import {
   accountLoginModel,
 } from '../../../protocols/db/account/load-account-by-access-token-repository'
 import { DebGetBookBuyList } from './db-get-book-buy-list'
+import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
+import { GetBookBuyListRepository } from '../../../protocols/db/book-buy-list/get-book-buy-list-repository'
+
+const makeFakeAddBuyBookModel = (): AddBuyBookModel => ({
+  pageCount: 1,
+  authors: ['any_author'],
+  amount: 0,
+  date: 0,
+  description: 'any_description',
+  title: 'any_title',
+  bookId: 'any_book_id',
+  id: 'any_id',
+  imgUrl: 'any_url',
+  language: 'any-language',
+  price: 0,
+  publisher: 'any_publisher',
+  publisherDate: 'any_date',
+  queryDoc: 'any_id_doc',
+  userId: 'any_user_id',
+})
+
+const makeGetBuyBookListRepositoryStub = (): GetBookBuyListRepository => {
+  class GetBookBuyListRepositoryStub implements GetBookBuyListRepository {
+    async getBook(
+      userId: string,
+      bookId: string
+    ): Promise<AddBuyBookModel | null> {
+      return await Promise.resolve(makeFakeAddBuyBookModel())
+    }
+  }
+  return new GetBookBuyListRepositoryStub()
+}
 
 const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
   class LoadAccountStub implements LoadAccountByAccessTokenRepository {
@@ -23,14 +55,20 @@ const makeLoadAccountStub = (): LoadAccountByAccessTokenRepository => {
 }
 
 interface SutTypes {
+  getBookBuyListRepositoryStub: GetBookBuyListRepository
   loadAccountStub: LoadAccountByAccessTokenRepository
   sut: DebGetBookBuyList
 }
 
 const makeSut = (): SutTypes => {
+  const getBookBuyListRepositoryStub = makeGetBuyBookListRepositoryStub()
   const loadAccountStub = makeLoadAccountStub()
-  const sut = new DebGetBookBuyList(loadAccountStub)
+  const sut = new DebGetBookBuyList(
+    loadAccountStub,
+    getBookBuyListRepositoryStub
+  )
   return {
+    getBookBuyListRepositoryStub,
     loadAccountStub,
     sut,
   }
@@ -60,5 +98,12 @@ describe('DebGetBookBuyList', () => {
       .mockReturnValueOnce(Promise.reject(new Error()))
     const promise = sut.getBook('any_token', 'any_book_id')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should call getBook with correct values', async () => {
+    const { getBookBuyListRepositoryStub, sut } = makeSut()
+    const loadSpy = jest.spyOn(getBookBuyListRepositoryStub, 'getBook')
+    await sut.getBook('any_token', 'any_book_id')
+    expect(loadSpy).toHaveBeenCalledWith('any_user_id', 'any_book_id')
   })
 })
