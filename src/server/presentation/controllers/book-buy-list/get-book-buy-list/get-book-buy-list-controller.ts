24765610dc45b1c6ed13347876cabdd82fdfa763
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-27 15:35:35 -0300

    feat: ensure GetBookBuyListController call GetBuyBook with correct value

--- src/server/presentation/controllers/book-buy-list/get-book-buy-list/get-book-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/get-book-buy-list/get-book-buy-list-controller.ts
@@ -1,15 +1,21 @@
+import { GetBookBuyList } from '../../../../domain/usecases/book-buy-list/get-book-buy-list'
 import { badRequest, ok } from '../../../helpers/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 
 export class GetBookBuyListController implements Controller {
-  constructor(private readonly validate: Validation) {}
+  constructor(
+    private readonly validate: Validation,
+    private readonly getBuyBook: GetBookBuyList
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validate.validation(httpRequest)
     if (error) {
       return badRequest(error)
     }
+    const { accessToken, bookId } = httpRequest.body
+    await this.getBuyBook.getBook(accessToken, bookId)
     return ok('success')
   }
 }
