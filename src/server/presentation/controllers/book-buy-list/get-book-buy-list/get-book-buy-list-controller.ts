05adbbfdaddb221b405ceb8ce824f7b2c6697a54
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-27 15:46:42 -0300

    feat: ensure GetBookBuyListController return null if GetBuyBook return null

--- src/server/presentation/controllers/book-buy-list/get-book-buy-list/get-book-buy-list-controller.ts
+++ src/server/presentation/controllers/book-buy-list/get-book-buy-list/get-book-buy-list-controller.ts
@@ -19,6 +19,6 @@ export class GetBookBuyListController implements Controller {
     if (book === undefined) {
       return unauthorized()
     }
-    return ok('success')
+    return ok(book)
   }
 }
