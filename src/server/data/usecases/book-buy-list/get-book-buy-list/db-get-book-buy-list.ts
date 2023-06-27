999fc568da61cc5cc310c6d7814f9631f1652a53
jefferson guirra <jeffersonguirra8@gmail.com>
2023-06-27 16:12:08 -0300

    feat: ensure DebGetBookBuyList call LoadAccount with correct token

---
+++ src/server/data/usecases/book-buy-list/get-book-buy-list/db-get-book-buy-list.ts
@@ -0,0 +1,16 @@
+import { AddBuyBookModel } from '../../../../domain/usecases/book-buy-list/add-book-buy-list'
+import { GetBookBuyList } from '../../../../domain/usecases/book-buy-list/get-book-buy-list'
+import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
+
+export class DebGetBookBuyList implements GetBookBuyList {
+  constructor(
+    private readonly loadAccount: LoadAccountByAccessTokenRepository
+  ) {}
+  async getBook(
+    accessToken: string,
+    bookId: string
+  ): Promise<AddBuyBookModel | null | undefined> {
+    await this.loadAccount.loadByAccessToken(accessToken)
+    return
+  }
+}
