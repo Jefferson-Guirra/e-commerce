0b704dbb090b24611958857c7137fbce0b2c1064
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-01 12:08:53 -0300

    feat: integrate nextauh in database

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,10 @@
-import { getSession } from 'next-auth/react'
-import { SessionUser } from '../../Types/User'
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
 import { LoginContainer } from '../../features'
 import nookies from 'nookies'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]'
+import { MongoHelper } from '../../server/infra/db/helpers/mongo-helper'
 
 const Login = () => {
   return (
@@ -19,7 +20,7 @@ const Login = () => {
 export default Login
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = (await getSession({ req: ctx.req })) as SessionUser
+  const session = await getServerSession(ctx.req, ctx.res, authOptions)
   const { literando_accessToken } = nookies.get(ctx)
 
   if (literando_accessToken) {
@@ -30,7 +31,32 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       },
     }
   }
-  if (session?.id) {
+  if (session) {
+    const { accessToken, name, email } = session.user
+    nookies.set(ctx, 'literando_accessToken', JSON.stringify(accessToken), {
+      maxAge: 86400 * 7,
+      page: '/',
+    })
+    nookies.set(ctx, 'literando_username', JSON.stringify(String(name)), {
+      maxAge: 86400 * 7,
+      path: '/',
+    })
+    MongoHelper.uri = process.env.MONGO_URL as string
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountsCollection.findOne({ email })
+    if (!account) {
+      await accountsCollection.insertOne({ email, accessToken, username: name })
+    } else {
+      await accountsCollection.updateOne(
+        { email },
+        {
+          $set: {
+            accessToken,
+          },
+        }
+      )
+    }
+
     return {
       redirect: {
         destination: '/',
@@ -38,6 +64,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       },
     }
   }
+
   return {
     props: {},
   }
