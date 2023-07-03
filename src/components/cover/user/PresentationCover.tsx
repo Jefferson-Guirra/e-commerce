4cee27d5a28a465c9cd3bf970c44c1ec887de1aa
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-03 15:15:08 -0300

    feat: create PresentationCover component

---
+++ src/components/cover/user/PresentationCover.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from './styles.module.css'
+export const PresentationCover = () => {
+  return (
+    <article className={styles.container}>
+      <Image
+        src={'/images/presentation-cover.jpg'}
+        quality={100}
+        width={1500}
+        height={0}
+        style={{ width: '100%', height: '100%' }}
+        alt="presentation image"
+      />
+    </article>
+  )
+}
