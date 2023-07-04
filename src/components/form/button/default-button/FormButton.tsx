8d8a05f10cf4a2f07eb4caa868a99b51b91f7fbe
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-04 18:26:06 -0300

    feat: add iconProps in FormButton component

--- src/components/form/button/default-button/FormButton.tsx
+++ src/components/form/button/default-button/FormButton.tsx
@@ -2,14 +2,27 @@ import { ButtonHTMLAttributes, ElementType, ReactNode } from 'react'
 import styles from '../styles.module.css'
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string
+  iconProps?: {
+    Icon: ElementType
+    size: number
+    color: string
+  }
   children: ReactNode
 }
-export const FormButton = ({ text, children, ...rest }: Props) => {
+export const FormButton = ({
+  text,
+  children,
+  iconProps: IconProps,
+  ...rest
+}: Props) => {
   return (
     <>
       {!rest.disabled ? (
         <button className={styles.btn} {...rest}>
           {text}
+          {IconProps && (
+            <IconProps.Icon size={IconProps.size} color={IconProps.color} />
+          )}
         </button>
       ) : (
         <button className={styles.btn} {...rest}>
