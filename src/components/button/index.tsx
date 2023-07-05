8faa8594b3e12a6eba80db573917be0eea26f222
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-04 23:45:27 -0300

    feat: create button component with composite pattern

---
+++ src/components/button/index.tsx
@@ -0,0 +1,7 @@
+import { ButtonDefault } from './default/ButtonDefault'
+import { ButtonIcon } from './icon/ButtonIcon'
+
+export const ButtonComposite = {
+  Default: ButtonDefault,
+  Icon: ButtonIcon,
+}
