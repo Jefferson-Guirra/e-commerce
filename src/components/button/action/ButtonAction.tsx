a0a3115f5724ef43a16681ff2d7d5eadaf950f20
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-05 01:22:41 -0300

    feat: create ButtonAction component

---
+++ src/components/button/action/ButtonAction.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes, ElementType, ReactNode } from 'react'
+import styles from '../default/styles.module.css'
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string
+  children: ReactNode
+}
+
+export const ButtonAction = ({ text, children, disabled, ...rest }: Props) => {
+  return (
+    <div className={styles.container}>
+      {!disabled ? (
+        <button {...rest}>{text}</button>
+      ) : (
+        <button disabled {...rest}>
+          {children}
+        </button>
+      )}
+    </div>
+  )
+}
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,9 @@
+import { ButtonAction } from './action/ButtonAction'
 import { ButtonDefault } from './default/ButtonDefault'
 import { ButtonIcon } from './icon/ButtonIcon'
 
 export const ButtonComposite = {
   Default: ButtonDefault,
+  Action: ButtonAction,
   Icon: ButtonIcon,
 }
