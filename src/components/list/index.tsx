028538a0ed271f6dcdba8830b69d77d0b47edd3d
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-06 00:05:02 -0300

    feat: create ListDataBook component  with composite pattern

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,19 +1,25 @@
 import { ListButton } from './button/ListButton'
+import { ListCard } from './card/ListCard'
+import { ListInfo } from './content/info/ListInfo'
 import { ListHeader } from './header/Header'
 import { ListHeaderCard } from './header/card/ListHeaderCard'
 import { ListIcon } from './icon/ListIcon'
-import ListImg from './img/ListImg'
+import { ListImg } from './img/ListImg'
+import { ListDataBook } from './info/data-book/ListDataBook'
 import { ListShipping } from './info/shipping/ListShipping'
 import { ListInput } from './input/ListInput'
-import ListLink from './link/ListLink'
+import { ListLink } from './link/ListLink'
 
 export const List = {
   Button: ListButton,
+  Card: ListCard,
+  Info: ListInfo,
   Header: ListHeader,
   Icon: ListIcon,
   HeaderCard: ListHeaderCard,
   Input: ListInput,
   Link: ListLink,
   Img: ListImg,
+  Data: ListDataBook,
   Shipping: ListShipping,
 }
