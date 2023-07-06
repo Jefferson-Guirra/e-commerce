624b6bf1a3eafa0b5cc327432fefc0465005839f
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-06 00:03:53 -0300

    feat: create ListDataBook component  with composite pattern

---
+++ src/components/list/info/data-book/ListDataBook.tsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.css'
+interface Props {
+  title: string
+  price: number
+  page: number
+  language: string
+}
+const formatPrice = (number: number) => {
+  return number.toLocaleString('pt-BR')
+}
+export const ListDataBook = ({ title, price, page, language }: Props) => {
+  return (
+    <div className={styles.container}>
+      <p>
+        <span>Título:</span> {title}
+      </p>
+      <p>
+        <span>Preço:</span> {formatPrice(price)}
+      </p>
+      <p>
+        <span>Páginas:</span> {page}
+      </p>
+      <p>
+        <span>Idioma:</span> {language}
+      </p>
+    </div>
+  )
+}
