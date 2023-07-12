b3d77b68a43a40173e04a38b4c0a86c5eb7ab659
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-11 22:27:47 -0300

    feat: ensure EmailValidatorAdapter call Validator witch correct value

--- src/server/main/adapters/validators/email-validator.spec.ts
+++ src/server/main/adapters/validators/email-validator.spec.ts
@@ -13,12 +13,19 @@ describe('EmailValidatorAdapter', () => {
   test('should return false if validator return false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('invalid_email@gmail.com')
+    const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBeFalsy()
   })
   test('should return true if validator return true', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('valid_email@gmail.com')
+    const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBeTruthy()
+  })
+
+  test('should  call validator with correct email', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toBeCalledWith('any_email@mail.com')
   })
 })
