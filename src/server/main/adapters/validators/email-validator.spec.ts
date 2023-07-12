c1ebefa2d1e5ca0524e0ddbe5c22cafc233731ba
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-11 22:10:56 -0300

    feat: ensure EmailValidatorAdapter return true if Validator return true

--- src/server/main/adapters/validators/email-validator.spec.ts
+++ src/server/main/adapters/validators/email-validator.spec.ts
@@ -13,7 +13,12 @@ describe('EmailValidatorAdapter', () => {
   test('should return false if validator return false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('any_email@gmail.com')
+    const isValid = sut.isValid('invalid_email@gmail.com')
     expect(isValid).toBeFalsy()
+  })
+  test('should return true if validator return true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@gmail.com')
+    expect(isValid).toBeTruthy()
   })
 })
