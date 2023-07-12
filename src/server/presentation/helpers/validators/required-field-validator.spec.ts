0b3d874097607e1e9b59b8e8dca7813d9cfd729a
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-12 15:40:03 -0300

    test: ensuere RequiredFieldValidator return undefined if validation success

--- src/server/presentation/helpers/validators/required-field-validator.spec.ts
+++ src/server/presentation/helpers/validators/required-field-validator.spec.ts
@@ -2,13 +2,21 @@ import { MissingParamError } from '../../errors/missing-params-error'
 import { HttpRequest } from '../../protocols/http'
 import { RequiredFieldValidator } from './required-field-validator'
 
-const makeFakeRequest = (): HttpRequest => ({
-  body: {
+const makeFakeRequest = (fieldName?: string): HttpRequest => {
+  const body: any = {
     username: 'any_username',
     password: 'any_password',
     email: 'any_email@mail.com',
-  },
-})
+  }
+
+  if (fieldName) {
+    body[fieldName] = 'any_field'
+  }
+
+  return {
+    body,
+  }
+}
 
 interface SutTypes {
   sut: RequiredFieldValidator
@@ -26,5 +34,11 @@ describe('RequiredFieldValidator', () => {
     const { sut } = makeSut()
     const error = sut.validation(makeFakeRequest())
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('should return undefined if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validation(makeFakeRequest('field'))
+    expect(error).toBeFalsy()
   })
 })
