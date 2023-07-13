0ecbb6b0dc4d6ba9d47a1ec5ee7ae96c9bb89a59
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-13 13:54:14 -0300

    feat: add ValidatorComposite in addBookListController

--- src/server/main/factories/book-list/add-book-list/add-book-list-factory.ts
+++ src/server/main/factories/book-list/add-book-list/add-book-list-factory.ts
@@ -4,11 +4,11 @@ import { BookListMongoRepository } from '../../../../infra/db/book-list/book-lis
 import { LogMongoRepository } from '../../../../infra/db/log/log-mongo-repository'
 import { AddBookListController } from '../../../../presentation/controllers/book-list/add-book-list/add-book-list-controller'
 import { Controller } from '../../../../presentation/protocols/controller'
-import { AddBookListValidator } from '../../../../presentation/helpers/validators/book-list/add-book-list-validator'
 import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
+import { makeAddBookValidator } from './add-book-list-validator-factory'
 
 export const makeAddBookListController = (): Controller => {
-  const validate = new AddBookListValidator()
+  const validate = makeAddBookValidator()
   const accountMongoRepository = new AccountMongoRepository()
   const bookListMongoRepository = new BookListMongoRepository()
   const logMongoRepository = new LogMongoRepository()
