534b088315130139d36a285ffceefeb138e1aa6f
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-13 15:30:14 -0300

    refactor: move makeGetBuyBooksListController file to get-books folder

--- src/pages/api/buybooklist/index.ts
+++ src/pages/api/buybooklist/index.ts
@@ -1,4 +1,4 @@
 import { adapterRouter } from '../../../server/main/adapters/next/adapter-route'
-import { makeGetBuyBooksListController } from '../../../server/main/factories/book-buy-list/get-books-buy-list-factory'
+import { makeGetBuyBooksListController } from '../../../server/main/factories/book-buy-list/get-books/get-books-buy-list-factory'
 
 export default adapterRouter(makeGetBuyBooksListController())
