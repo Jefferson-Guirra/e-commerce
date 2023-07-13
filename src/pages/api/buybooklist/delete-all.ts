fafa435572e3dae4a9d2e6a01d19312a9ea824af
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-13 15:34:36 -0300

    refactor: move makeRemoveAllBooksBuyListController file  to remove-all-books folder

--- src/pages/api/buybooklist/delete-all.ts
+++ src/pages/api/buybooklist/delete-all.ts
@@ -1,4 +1,4 @@
 import { adapterRouter } from '../../../server/main/adapters/next/adapter-route'
-import { makeDeleteAllBuyBooksListController } from '../../../server/main/factories/book-buy-list/delete-all-book-buy-list-controller-factory'
+import { makeRemoveAllBooksBuyListController } from '../../../server/main/factories/book-buy-list/remove-all-books/remove-all-book-buy-list-controller-factory'
 
-export default adapterRouter(makeDeleteAllBuyBooksListController())
+export default adapterRouter(makeRemoveAllBooksBuyListController())
