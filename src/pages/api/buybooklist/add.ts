6449f1f1fe0b1e9a9f039c54c31e38041cb605c0
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-13 15:13:35 -0300

    refactor: move makeAddBookBuyListController file to add-book folder

--- src/pages/api/buybooklist/add.ts
+++ src/pages/api/buybooklist/add.ts
@@ -1,4 +1,4 @@
 import { adapterRouter } from '../../../server/main/adapters/next/adapter-route'
-import { makeAddBookBuyListController } from '../../../server/main/factories/book-buy-list/add-book-buy-list-controller-factory'
+import { makeAddBookBuyListController } from '../../../server/main/factories/book-buy-list/add-book/add-book-buy-list-controller-factory'
 
 export default adapterRouter(makeAddBookBuyListController())
