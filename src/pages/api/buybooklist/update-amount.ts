004658a55cd574bbf129dc48d6b99be70f2c3ee1
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-13 15:27:26 -0300

    refactor: move makeUpdateAmountBookBuyListController file to update-amount folder

--- src/pages/api/buybooklist/update-amount.ts
+++ src/pages/api/buybooklist/update-amount.ts
@@ -1,4 +1,4 @@
 import { adapterRouter } from '../../../server/main/adapters/next/adapter-route'
-import { makeUpdateAmountBookBuyListController } from '../../../server/main/factories/book-buy-list/update-amount-book-buy-list-controller-factory'
+import { makeUpdateAmountBookBuyListController } from '../../../server/main/factories/book-buy-list/update-amount/update-amount-book-buy-list-controller-factory'
 
 export default adapterRouter(makeUpdateAmountBookBuyListController())
---
+++ src/server/main/factories/book-buy-list/update-amount/update-amount-book-buy-list-controller-factory.ts
@@ -0,0 +1,21 @@
+import { DbUpdateAmountBookBuyList } from '../../../../data/usecases/book-buy-list/update-amount-book-buy-list/db-update-amount-book-buy-list'
+import { AccountMongoRepository } from '../../../../infra/db/account/account-mongo-repository'
+import { BuyBooksListMongoRepository } from '../../../../infra/db/book-buy-list/book-buy-list-mongo-repository'
+import { UpdateAmountBookBuyListController } from '../../../../presentation/controllers/book-buy-list/update-amount-book-buy-list/update-amount-book-buy-list-controller'
+import { Controller } from '../../../../presentation/protocols/controller'
+import { UpdateAmountBookBuyListValidator } from '../../../../presentation/helpers/validators/book-buy-list/add-amount-book-buy-list-validator'
+
+export const makeUpdateAmountBookBuyListController = (): Controller => {
+  const validator = new UpdateAmountBookBuyListValidator()
+  const accountMongoRepository = new AccountMongoRepository()
+  const bookBuyListMongoRepository = new BuyBooksListMongoRepository()
+  const dbUpdateAmountBookBuyList = new DbUpdateAmountBookBuyList(
+    accountMongoRepository,
+    bookBuyListMongoRepository,
+    bookBuyListMongoRepository
+  )
+  return new UpdateAmountBookBuyListController(
+    validator,
+    dbUpdateAmountBookBuyList
+  )
+}
