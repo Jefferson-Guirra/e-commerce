abc43369e1734c125e92b4a5c6c77c173709772f
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-14 11:04:57 -0300

    test: ensure CompareFieldsValidator return invalidParamsError if Validation fails

---
+++ src/server/presentation/helpers/validators/compare-fields-validator.spec.ts
@@ -0,0 +1,23 @@
+import { InvalidParamsError } from '../../errors/invalid-params-error'
+import { HttpRequest } from '../../protocols/http'
+import { compareFieldsValidator } from './compare-fields-validator'
+
+const makeFakeRequest = (changeValue?: string): HttpRequest => {
+  const body = {
+    field: '123',
+    fieldToCompare: '123',
+  }
+  if (changeValue) body.fieldToCompare = changeValue
+
+  return { body }
+}
+
+const makeSut = () => new compareFieldsValidator('field', 'fieldToCompare')
+
+describe('compareFieldsValidator', () => {
+  test('should  return invalidParamsError if Validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validation(makeFakeRequest('12345'))
+    expect(error).toEqual(new InvalidParamsError('fieldToCompare'))
+  })
+})
