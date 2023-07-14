4211ef93e34ffdb3b60f579d6ca8a01aefcbbead
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-13 21:51:32 -0300

    feat: add RequiredFieldValidator  in GetBookBuyListController factory method

--- src/server/main/factories/book-buy-list/get-books/get-books-buy-list-factory.ts
+++ src/server/main/factories/book-buy-list/get-books/get-books-buy-list-factory.ts
@@ -3,10 +3,10 @@ import { AccountMongoRepository } from '../../../../infra/db/account/account-mon
 import { BuyBooksListMongoRepository } from '../../../../infra/db/book-buy-list/book-buy-list-mongo-repository'
 import { GetBuyBooksController } from '../../../../presentation/controllers/book-buy-list/get-books-buy-list/get-books-buy-list-controller'
 import { Controller } from '../../../../presentation/protocols/controller'
-import { GetBuyBooksValidator } from '../../../../presentation/helpers/validators/book-buy-list/get-books-buy-list-validator'
+import { RequiredFieldValidator } from '../../../../presentation/helpers/validators/required-field-validator'
 
 export const makeGetBuyBooksListController = (): Controller => {
-  const buyBooksValidator = new GetBuyBooksValidator()
+  const buyBooksValidator = new RequiredFieldValidator('accessToken')
   const accountMongoRepository = new AccountMongoRepository()
   const buyBookListMongoRepository = new BuyBooksListMongoRepository()
   const dbGetBuyBooks = new DbGetBuyBooks(
