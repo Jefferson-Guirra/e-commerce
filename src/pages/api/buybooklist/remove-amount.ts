30448d80d985f6cfea35bdb7bd4ce0444f135f8a
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-14 10:25:22 -0300

    refactor: remove unused route

--- src/pages/api/buybooklist/remove-amount.ts
+++
@@ -1,4 +0,0 @@
-import { adapterRouter } from '../../../server/main/adapters/next/adapter-route'
-import { makeRemoveAmountBookBuyListController } from '../../../server/main/factories/book-buy-list/remove-amount-book-buy-list-factory'
-
-export default adapterRouter(makeRemoveAmountBookBuyListController())
