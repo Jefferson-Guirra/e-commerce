04fd0427e4c6b61ec749aff0cb716b379ee6c404
Jefferson Guirra <jeffersonguirra8@gmail.com>
2024-03-23 11:20:48 -0300

    feat: add user actions on MenuMobile component

--- src/components/header/menu-mobile/MenuMobile.tsx
+++ src/components/header/menu-mobile/MenuMobile.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 import styles from './styles.module.css'
 import { BiUser } from 'react-icons/bi'
 import { parseCookies } from 'nookies'
-import { FiShoppingCart } from 'react-icons/fi'
+import { signOut } from 'next-auth/react'
+import { HandleCookies } from '../../../utils/handle-cookie'
+import { Api } from '../../../utils/api'
+import Link from 'next/link'
 import { useHeaderContext } from '../../../context/header/HeaderContext'
 
+const userApi = new Api()
+const handleCookies = new HandleCookies()
+
 export const MenuMobile = () => {
-  const { amountList, menuMob, username } = useHeaderContext()
+  const { amountList, menuMob, username, dispatch } = useHeaderContext()
+
+  const handleLogout = async () => {
+    const accessToken = JSON.parse(parseCookies().literando_accessToken)
+    await userApi.send('logout', 'PUT', { accessToken })
+    handleCookies.destroyCookie('literando_accessToken')
+    handleCookies.destroyCookie('literando_username')
+    signOut()
+  }
 
   return (
     <article
@@ -16,28 +30,63 @@ export const MenuMobile = () => {
     >
       <ul className={styles.list}>
         <li>
-          <a href="#">
+          <Link
+            href="/"
+            onClick={() => dispatch({ type: 'REVERT_MENU_MOB_STATE' })}
+          >
             <BiUser className={styles.user} size={23} />
             Olá {username === 'undefined' ? 'usuário' : username.toLowerCase()}
-          </a>
+          </Link>
+        </li>
+        {username === 'undefined' && (
+          <li>
+            <Link
+              href="/Login"
+              onClick={() => dispatch({ type: 'REVERT_MENU_MOB_STATE' })}
+            >
+              Entrar
+            </Link>
+          </li>
+        )}
+        {username === 'undefined' && (
+          <li>
+            <Link
+              href="/SignUp"
+              onClick={() => dispatch({ type: 'REVERT_MENU_MOB_STATE' })}
+            >
+              Cadastrar
+            </Link>
+          </li>
+        )}
+        {username !== 'undefined' && (
+          <li>
+            <Link
+              href="#"
+              onClick={() => {
+                handleLogout()
+                dispatch({ type: 'REVERT_MENU_MOB_STATE' })
+              }}
+            >
+              Sair
+            </Link>
+          </li>
+        )}
+        <li>
+          <Link
+            href="/List"
+            onClick={() => dispatch({ type: 'REVERT_MENU_MOB_STATE' })}
+          >
+            Minha Lista
+          </Link>
         </li>
         <li>
-          <a href="#">Entrar</a>
-        </li>
-        <li>
-          <a href="#">Cadastrar</a>
-        </li>
-        <li>
-          <a href="#">Sair</a>
-        </li>
-        <li>
-          <a href="#">Minha Lista</a>
-        </li>
-        <li>
-          <a href="#">
+          <Link
+            href="Buy"
+            onClick={() => dispatch({ type: 'REVERT_MENU_MOB_STATE' })}
+          >
             <span className={styles['number-items']}>{amountList}</span>
             Meu Carrinho
-          </a>
+          </Link>
         </li>
       </ul>
     </article>
